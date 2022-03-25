89c0160e25850993287433d14a856287535d3739
Bitard Michaël <bitard.michael@gmail.com>
2022-03-25 11:57:03 +0100

    chore(): ajoute les définitions en dur

--- packages/ui/src/components/glossaire.vue
+++ packages/ui/src/components/glossaire.vue
@@ -6,7 +6,7 @@
 
     <div class="desktop-blobs">
       <div class="desktop-blob-1-3">
-        <Sommaire :slug="slug" :definitions="definitions" />
+        <Sommaire />
       </div>
       <div class="desktop-blob-2-3">
         <Router-view v-if="slug" :slug="slug" :definition="definition" />
@@ -18,45 +18,22 @@
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import Sommaire from './glossaire/sommaire.vue'
+import {definitions} from "./glossaire/definitions";
+import {useRoute} from "vue-router";
+import {ref, watch} from 'vue'
+const route = useRoute()
 
-export default {
-  name: 'Glossaire',
-
-  components: { Sommaire },
-
-  data() {
-    return {
-      loaded: false
-    }
-  },
-
-  computed: {
-    definitions() {
-      return this.$store.state.definitions.elements
-    },
-
-    definition() {
-      return this.definitions.find(d => d.slug === this.slug) || {}
-    },
-
-    slug() {
-      return this.$route.params.slug
-    }
-  },
-
-  async created() {
-    await this.definitionsGet()
-  },
-
-  methods: {
-    async definitionsGet() {
-      await this.$store.dispatch('definitions/get')
-      if (!this.loaded) {
-        this.loaded = true
-      }
-    }
+const slug = ref<string>(route.params.slug as string)
+const definition = ref({})
+watch(() => route.params.slug, (newSlug) => {
+  const definitionFound = definitions.find(d => d.slug === newSlug);
+  if (definitionFound) {
+    slug.value = definitionFound.slug
+    definition.value = definitionFound
+    console.log('set definition', definition)
   }
-}
+})
+
 </script>
