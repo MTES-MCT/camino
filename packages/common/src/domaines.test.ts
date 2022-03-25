7b9ea7b2ed94ac1853c4aeb81be7d7e7e74b9417
Bitard Michaël <bitard.michael@gmail.com>
2022-03-25 10:42:06 +0100

    separate lint and format

--- packages/common/src/domaines.test.ts
+++ packages/common/src/domaines.test.ts
@@ -1,7 +1,7 @@
-import test from "ava";
-import { Domaines, DOMAINES_IDS } from "./domaines";
+import test from 'ava'
+import { Domaines, DOMAINES_IDS } from './domaines'
 
-test("domaine", (t) => {
-  const domaine = DOMAINES_IDS[0];
-  t.is(Domaines.c, Domaines[domaine]);
-});
+test('domaine', t => {
+  const domaine = DOMAINES_IDS[0]
+  t.is(Domaines.c, Domaines[domaine])
+})
