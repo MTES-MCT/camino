MTES-MCT/camino-api

feat(activités): envoie des notifications aux opérateurs à propos de leurs activités à renseigner

src/business/processes/titres-activites-relance-send.test.ts
@@ -1,0 +1,68 @@
+import { mocked } from 'ts-jest/utils'
+
+import { titresActivitesGet } from '../../database/queries/titres-activites'
+
+import TitresActivites from '../../database/models/titres-activites'
+import {
+  ACTIVITES_DELAI_RELANCE_JOURS,
+  titresActivitesRelanceSend
+} from './titres-activites-relance-send'
+import { dateAddDays, dateAddMonths } from '../../tools/date'
+import dateFormat from 'dateformat'
+import { emailsSend } from '../../tools/api-mailjet/emails'
+
+jest.mock('../../database/queries/titres-activites', () => ({
+  titresActivitesGet: jest.fn()
+}))
+
+jest.mock('../../tools/api-mailjet/emails', () => ({
+  __esModule: true,
+  emailsSend: jest.fn().mockImplementation(a => a)
+}))
+
+const titresActivitesGetMock = mocked(titresActivitesGet, true)
+const emailsSendMock = mocked(emailsSend, true)
+
+console.info = jest.fn()
+
+describe('relance les opérateurs des activités qui vont se fermer automatiquement', () => {
+  test('envoie un email aux opérateurs', async () => {
+    const delaiMois = 3
+
+    const today = dateFormat(new Date(), 'yyyy-mm-dd')
+    let date = dateAddDays(today, ACTIVITES_DELAI_RELANCE_JOURS)
+    date = dateAddMonths(date, -delaiMois)
+
+    const email = 'toto.huhu@foo.com'
+
+    titresActivitesGetMock.mockResolvedValue([
+      {
+        date,
+        type: { delaiMois },
+        titre: {
+          titulaires: [{ utilisateurs: [{ email: 'toto.huhu@foo.com' }] }]
+        }
+      }
+    ] as TitresActivites[])
+    const titresActivites = await titresActivitesRelanceSend()
+
+    expect(emailsSendMock).toBeCalledWith(
+      [email],
+      expect.any(String),
+      expect.any(String)
+    )
+    expect(titresActivites.length).toEqual(1)
+  })
+
+  test('n’envoie pas d’email aux opérateurs', async () => {
+    titresActivitesGetMock.mockResolvedValue([
+      {
+        date: '1000-01-01',
+        type: { delaiMois: 3 }
+      }
+    ] as TitresActivites[])
+    const titresActivites = await titresActivitesRelanceSend()
+
+    expect(titresActivites.length).toEqual(0)
+  })
+})

src/business/processes/titres-activites-relance-send.ts
@@ -1,0 +1,80 @@
+import dateFormat from 'dateformat'
+
+import { titresActivitesGet } from '../../database/queries/titres-activites'
+import { userSuper } from '../../database/user-super'
+import { dateAddDays } from '../../tools/date'
+import { titreActiviteDateDelaiFind } from '../rules/titre-activite-statut-id-find'
+import {
+  emailsWithTemplateSend,
+  IEmailTemplateId
+} from '../../tools/api-mailjet/emails'
+import { activitesUrlGet } from '../utils/urls-get'
+
+export const ACTIVITES_DELAI_RELANCE_JOURS = 14
+
+const titresActivitesRelanceSend = async () => {
+  console.info()
+  console.info('relance des activités des titres…')
+
+  const statutsIds = ['abs', 'enc']
+  const typesIds = ['gra', 'grx', 'pma', 'pmb', 'pmc', 'pmd', 'wrp']
+  const activites = await titresActivitesGet(
+    { statutsIds, typesIds },
+    {
+      fields: {
+        type: { id: {} },
+        titre: { titulaires: { utilisateurs: { id: {} } } }
+      }
+    },
+    userSuper
+  )
+
+  const aujourdhui = dateFormat(new Date(), 'yyyy-mm-dd')
+
+  const dateDelai = dateAddDays(aujourdhui, ACTIVITES_DELAI_RELANCE_JOURS)
+
+  // FIXME 3 mois
+  const titresActivitesRelanceToSend = activites.filter(
+    ({ date }) => dateDelai === titreActiviteDateDelaiFind(date, 3)
+  )
+
+  if (titresActivitesRelanceToSend.length) {
+    // envoi d’email aux opérateurs pour les relancer ACTIVITES_DELAI_RELANCE_JOURS jours avant la fermeture automatique de l’activité
+    const emails = new Set<string>()
+    for (const activite of titresActivitesRelanceToSend) {
+      const titre = activite.titre!
+      titre.titulaires?.forEach(titulaire =>
+        titulaire.utilisateurs?.forEach(({ email }) => {
+          if (email) {
+            emails.add(email)
+          }
+        })
+      )
+    }
+
+    if (emails.size) {
+      await emailsWithTemplateSend(
+        [...emails],
+        IEmailTemplateId.ACTIVITES_RELANCE,
+        {
+          activitesUrl: activitesUrlGet({
+            typesIds,
+            statutsIds,
+            annees: [new Date().getFullYear() - 1]
+          })
+        }
+      )
+    }
+
+    const log = {
+      type: 'titre / activités (relance) ->',
+      value: titresActivitesRelanceToSend.map(ta => ta.id).join(', ')
+    }
+
+    console.info(log.type, log.value)
+  }
+
+  return titresActivitesRelanceToSend.map(ta => ta.id)
+}
+
+export { titresActivitesRelanceSend }

MTES-MCT/camino

packages/api/src/business/processes/titres-activites-relance-send.test.ts
@@ -1,0 +1,69 @@
+import { mocked } from 'ts-jest/utils'
+
+import { titresActivitesGet } from '../../database/queries/titres-activites'
+
+import TitresActivites from '../../database/models/titres-activites'
+import {
+  ACTIVITES_DELAI_RELANCE_JOURS,
+  titresActivitesRelanceSend
+} from './titres-activites-relance-send'
+import { dateAddDays, dateAddMonths } from '../../tools/date'
+import dateFormat from 'dateformat'
+import { emailsWithTemplateSend } from '../../tools/api-mailjet/emails'
+import { EmailTemplateId } from '../../tools/api-mailjet/types'
+
+jest.mock('../../database/queries/titres-activites', () => ({
+  titresActivitesGet: jest.fn()
+}))
+
+jest.mock('../../tools/api-mailjet/emails', () => ({
+  __esModule: true,
+  emailsWithTemplateSend: jest.fn().mockImplementation(a => a)
+}))
+
+const titresActivitesGetMock = mocked(titresActivitesGet, true)
+const emailsWithTemplateSendMock = mocked(emailsWithTemplateSend, true)
+
+console.info = jest.fn()
+
+describe('relance les opérateurs des activités qui vont se fermer automatiquement', () => {
+  test('envoie un email aux opérateurs', async () => {
+    const delaiMois = 3
+
+    const today = dateFormat(new Date(), 'yyyy-mm-dd')
+    let date = dateAddDays(today, ACTIVITES_DELAI_RELANCE_JOURS)
+    date = dateAddMonths(date, -delaiMois)
+
+    const email = 'toto.huhu@foo.com'
+
+    titresActivitesGetMock.mockResolvedValue([
+      {
+        date,
+        type: { delaiMois },
+        titre: {
+          titulaires: [{ utilisateurs: [{ email: 'toto.huhu@foo.com' }] }]
+        }
+      }
+    ] as TitresActivites[])
+    const titresActivites = await titresActivitesRelanceSend()
+
+    expect(emailsWithTemplateSendMock).toBeCalledWith(
+      [email],
+      EmailTemplateId.ACTIVITES_RELANCE,
+      expect.any(Object)
+    )
+    expect(titresActivites.length).toEqual(1)
+  })
+
+  test('n’envoie pas d’email aux opérateurs', async () => {
+    titresActivitesGetMock.mockResolvedValue([
+      {
+        date: '1000-01-01',
+        type: { delaiMois: 3 }
+      }
+    ] as TitresActivites[])
+    const titresActivites = await titresActivitesRelanceSend()
+
+    expect(titresActivites.length).toEqual(0)
+  })
+})

packages/api/src/business/processes/titres-activites-relance-send.ts
@@ -1,0 +1,78 @@
+import dateFormat from 'dateformat'
+
+import { titresActivitesGet } from '../../database/queries/titres-activites'
+import { userSuper } from '../../database/user-super'
+import { dateAddDays } from '../../tools/date'
+import { titreActiviteDateDelaiFind } from '../rules/titre-activite-statut-id-find'
+import { emailsWithTemplateSend } from '../../tools/api-mailjet/emails'
+import { activitesUrlGet } from '../utils/urls-get'
+import { EmailTemplateId } from '../../tools/api-mailjet/types'
+
+export const ACTIVITES_DELAI_RELANCE_JOURS = 14
+
+const titresActivitesRelanceSend = async () => {
+  console.info()
+  console.info('relance des activités des titres…')
+
+  const statutsIds = ['abs', 'enc']
+  const typesIds = ['gra', 'grx', 'pma', 'pmb', 'pmc', 'pmd', 'wrp']
+  const activites = await titresActivitesGet(
+    { statutsIds, typesIds },
+    {
+      fields: {
+        type: { id: {} },
+        titre: { titulaires: { utilisateurs: { id: {} } } }
+      }
+    },
+    userSuper
+  )
+
+  const aujourdhui = dateFormat(new Date(), 'yyyy-mm-dd')
+
+  const dateDelai = dateAddDays(aujourdhui, ACTIVITES_DELAI_RELANCE_JOURS)
+
+  // FIXME 3 mois
+  const titresActivitesRelanceToSend = activites.filter(
+    ({ date }) => dateDelai === titreActiviteDateDelaiFind(date, 3)
+  )
+
+  if (titresActivitesRelanceToSend.length) {
+    // envoi d’email aux opérateurs pour les relancer ACTIVITES_DELAI_RELANCE_JOURS jours avant la fermeture automatique de l’activité
+    const emails = new Set<string>()
+    for (const activite of titresActivitesRelanceToSend) {
+      const titre = activite.titre!
+      titre.titulaires?.forEach(titulaire =>
+        titulaire.utilisateurs?.forEach(({ email }) => {
+          if (email) {
+            emails.add(email)
+          }
+        })
+      )
+    }
+
+    if (emails.size) {
+      await emailsWithTemplateSend(
+        [...emails],
+        EmailTemplateId.ACTIVITES_RELANCE,
+        {
+          activitesUrl: activitesUrlGet({
+            typesIds,
+            statutsIds,
+            annees: [new Date().getFullYear() - 1]
+          })
+        }
+      )
+    }
+
+    const log = {
+      type: 'titre / activités (relance) ->',
+      value: titresActivitesRelanceToSend.map(ta => ta.id).join(', ')
+    }
+
+    console.info(log.type, log.value)
+  }
+
+  return titresActivitesRelanceToSend.map(ta => ta.id)
+}
+
+export { titresActivitesRelanceSend }