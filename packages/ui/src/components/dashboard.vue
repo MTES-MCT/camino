a5acb2cc994631aeb598e58ef91600d106aa8bcb
vmaubert <v.maubert@code-troopers.com>
2022-04-25 15:03:07 +0200

    ajout des administrations dans le common

--- packages/ui/src/components/dashboard.vue
+++ packages/ui/src/components/dashboard.vue
@@ -4,6 +4,7 @@
     :getEntreprisesTitres="getEntreprisesTitres"
     :displayActivites="activites"
   />
+  <div v-if="isONF">Tableau ONF</div>
   <div v-else>Loading</div>
 </template>
 
@@ -13,6 +14,7 @@ import PureEntrepriseDashboard from '@/components/dashboard/pure-entreprise-dash
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { titres } from '@/api/titres'
+import { ADMINISTRATION_IDS } from 'camino-common/src/administrations'
 
 const store = useStore()
 const router = useRouter()
@@ -22,13 +24,20 @@ const activites = user?.sections?.activites ?? false
 
 const entreprisesIds: string[] = []
 const hasEntreprises: boolean = store.getters['user/hasEntreprises']
+
+const isONF: boolean =
+  store.getters['user/userIsAdmin'] &&
+  user.administrations.find(
+    ({ id }: { id: string }) =>
+      id === ADMINISTRATION_IDS['OFFICE NATIONAL DES FORÊTS']
+  )
 if (hasEntreprises) {
   // TODO 2022-03-17: type the store
   const entreprises = store.getters['user/user']?.entreprises ?? []
   entreprisesIds.push(
     ...entreprises.map((entreprise: { id: string }) => entreprise.id)
   )
-} else {
+} else if (!isONF) {
   store.commit('titres/reset')
   store.dispatch('titres/init')
   router.push({ name: 'titres' })
