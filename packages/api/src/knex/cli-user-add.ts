cb8e3884aa4e66ae30aae31402f33730a11c7b03
Bitard Michaël <bitard.michael@gmail.com>
2022-05-02 09:47:45 +0200

    fix(api): crée un utilisateur administrateur

--- packages/api/src/knex/cli-user-add.ts
+++ packages/api/src/knex/cli-user-add.ts
@@ -1,6 +1,7 @@
 import '../init'
 import { knex } from '../knex'
-const userAdd = require('./user-add')
+import { userAdd } from './user-add'
+import { IUtilisateur } from '../types'
 
 const run = async () => {
   const user = {
@@ -8,7 +9,7 @@ const run = async () => {
     email: process.env.ADMIN_EMAIL,
     permissionId: 'super',
     motDePasse: process.env.ADMIN_PASSWORD
-  }
+  } as IUtilisateur
 
   try {
     await userAdd(knex, user)
