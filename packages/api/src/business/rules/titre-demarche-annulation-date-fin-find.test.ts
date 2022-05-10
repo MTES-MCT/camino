e04b7e74c6b5ff672b2627d86162dab5953915b9
Michaël Bitard <bitard.michael@gmail.com>
2022-05-10 08:59:06 +0200

    fix(phase): une renonciation ne change pas la date de fin (#53)

--- packages/api/src/business/rules/titre-demarche-annulation-date-fin-find.test.ts
+++ packages/api/src/business/rules/titre-demarche-annulation-date-fin-find.test.ts
@@ -1,32 +1,74 @@
+import { ITitreEtape } from '../../types'
 import { titreDemarcheAnnulationDateFinFind } from './titre-demarche-annulation-date-fin-find'
 
-import {
-  titreDemarcheACOFaitEtapesDateFin,
-  titreDemarcheAnnulationEtapes,
-  titreDemarcheAnnulationEtapesDateFin,
-  titreDemarcheAnnulationEtapesSansDate
-} from './__mocks__/titre-demarche-annulation-date-fin-find'
-
 describe("date de fin d'une démarche d'annulation", () => {
-  test("retourne la date d'une démarche d'annulation si elle n'a pas de date de fin", () => {
-    expect(
-      titreDemarcheAnnulationDateFinFind(titreDemarcheAnnulationEtapes)
-    ).toEqual('2013-05-21')
-  })
+  test.each(['dex', 'dux', 'dim'])(
+    "retourne la date d'une démarche d'annulation si elle n'a pas de date de fin pour une %p",
+    typeId => {
+      const titreDemarcheAnnulationEtapes: ITitreEtape[] = [
+        {
+          id: 'h-cx-courdemanges-1988-ret01-dex01',
+          titreDemarcheId: 'h-cx-courdemanges-1988-ret01',
+          typeId,
+          statutId: 'acc',
+          ordre: 1,
+          date: '2013-05-21'
+        }
+      ]
+      expect(
+        titreDemarcheAnnulationDateFinFind(titreDemarcheAnnulationEtapes)
+      ).toEqual('2013-05-21')
+    }
+  )
 
-  test("retourne la date de fin d'une démarche d'annulation si elle existe", () => {
-    expect(
-      titreDemarcheAnnulationDateFinFind(titreDemarcheAnnulationEtapesDateFin)
-    ).toEqual('2013-05-25')
-  })
+  test.each(['dex', 'dux', 'dim'])(
+    "retourne la date de fin d'une démarche d'annulation si elle existe pour une %p",
+    typeId => {
+      const titreDemarcheAnnulationEtapesDateFin: ITitreEtape[] = [
+        {
+          id: 'h-cx-courdemanges-1988-ret01-dex01',
+          titreDemarcheId: 'h-cx-courdemanges-1988-ret01',
+          typeId,
+          statutId: 'acc',
+          ordre: 1,
+          date: '2013-05-21',
+          dateFin: '2013-05-25'
+        }
+      ]
+      expect(
+        titreDemarcheAnnulationDateFinFind(titreDemarcheAnnulationEtapesDateFin)
+      ).toEqual('2013-05-25')
+    }
+  )
 
   test("retourne null si l'étape n'a ni date, ni date de fin", () => {
+    const titreDemarcheAnnulationEtapesSansDate: ITitreEtape[] = [
+      {
+        id: 'h-cx-courdemanges-1988-ret01-dex01',
+        titreDemarcheId: 'h-cx-courdemanges-1988-ret01',
+        typeId: 'dex',
+        statutId: 'acc',
+        ordre: 1,
+        date: null
+      }
+    ]
     expect(
       titreDemarcheAnnulationDateFinFind(titreDemarcheAnnulationEtapesSansDate)
     ).toBeNull()
   })
 
   test("retourne la date de fin d'une ACO si elle existe", () => {
+    const titreDemarcheACOFaitEtapesDateFin: ITitreEtape[] = [
+      {
+        id: 'h-cx-courdemanges-1988-ret01-dex01',
+        titreDemarcheId: 'h-cx-courdemanges-1988-ret01',
+        typeId: 'aco',
+        statutId: 'fai',
+        ordre: 1,
+        date: '2013-05-21',
+        dateFin: '2013-05-25'
+      }
+    ]
     expect(
       titreDemarcheAnnulationDateFinFind(titreDemarcheACOFaitEtapesDateFin)
     ).toEqual('2013-05-25')
