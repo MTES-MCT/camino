aa58964079bbe5b6ce219c697afc6567b1d1424e
Bitard Michaël <bitard.michael@gmail.com>
2022-06-01 11:25:34 +0200

    add storybook build

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -3,7 +3,9 @@ const { mergeConfig } = require('vite')
 const appConfig = require('../vite.config.js')
 module.exports = {
   async viteFinal(config, { configType }) {
-    const mergeConfigValue = mergeConfig(config, appConfig)
+    const mergeConfigValue = mergeConfig(appConfig, config)
+    // supprime le vue plugin qui vient de vite.config.js
+    mergeConfigValue.plugins.shift()
     return mergeConfigValue
   },
   core: { builder: '@storybook/builder-vite' },
