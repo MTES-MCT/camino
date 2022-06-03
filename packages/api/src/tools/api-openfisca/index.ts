1122ad836d7efc7f3868357782896b51ec95a067
vmaubert <v.maubert@code-troopers.com>
2022-06-03 11:39:41 +0200

    supprime du code inutilisé

--- packages/api/src/tools/api-openfisca/index.ts
+++ packages/api/src/tools/api-openfisca/index.ts
@@ -1,17 +1,18 @@
 import fetch from 'node-fetch'
 
-/*
- eslint-disable camelcase
-*/
-
-type plopEntry = 'quantite_aurifere_kg' | 'surface_communale' | 'surface_totale'
-type plopRequest =
+type InputAttribute =
+  | 'quantite_aurifere_kg'
+  | 'surface_communale'
+  | 'surface_totale'
+type OutputAttribute =
   | 'redevance_communale_des_mines_aurifere_kg'
   | 'redevance_departementale_des_mines_aurifere_kg'
   | 'taxe_guyane'
 
-type Entries = Partial<Record<plopEntry, { [annee: string]: number | null }>>
-type Request = Partial<Record<plopRequest, { [annee: string]: null }>>
+type Entries = Partial<
+  Record<InputAttribute, { [annee: string]: number | null }>
+>
+type Request = Partial<Record<OutputAttribute, { [annee: string]: null }>>
 
 type Article = Entries & Request
 
@@ -46,7 +47,7 @@ export interface OpenfiscaRequest {
 export interface OpenfiscaResponse {
   articles: {
     [titreId_substance_commune: string]: Partial<
-      Record<plopRequest, { [annee: string]: number }>
+      Record<OutputAttribute, { [annee: string]: number }>
     >
   }
 }
@@ -75,62 +76,3 @@ export const apiOpenfiscaFetch = async (
 
   return result
 }
-
-/**
- * Calcule la redevance communale des mines aurifères
- * @param entreprises - liste des entreprises avec leur production d’or net en gramme par année
- * @param annees - liste des années sur lesquelles on souhaite calculer la redevance
- * @return la redevance à payer par entreprise et par année
- */
-const redevanceCommunaleMinesAurifiereGet = async (
-  entreprises: {
-    id: string
-    orNet: { [annee: string]: number }
-  }[],
-  annees: number[]
-): Promise<{
-  [entrepriseId: string]: { [annee: string]: number | null }
-} | null> => {
-  // construction de l’objet qui permet de dire à Openfisca ce que l’on souhaite récupérer
-  const redevanceCommunaleDesMinesAurifereKg = annees.reduce((acc, annee) => {
-    acc[annee] = null
-
-    return acc
-  }, {} as { [annee: string]: null })
-
-  const societes: OpenfiscaRequest = entreprises.reduce(
-    (acc, entreprise) => {
-      // conversion des grammes en kilogrammes
-      const orNetKg = Object.keys(entreprise.orNet).reduce(
-        (orNetKg, annee) => ({
-          ...orNetKg,
-          [annee]: entreprise.orNet[annee] / 1000
-        }),
-        {}
-      )
-
-      acc.articles[entreprise.id] = {
-        quantite_aurifere_kg: orNetKg,
-        redevance_communale_des_mines_aurifere_kg:
-          redevanceCommunaleDesMinesAurifereKg
-      }
-
-      return acc
-    },
-    { articles: {} } as OpenfiscaRequest
-  )
-
-  const result = (await apiOpenfiscaFetch(societes))?.articles
-
-  if (result) {
-    return Object.keys(result).reduce((acc, societe) => {
-      acc[societe] = result[societe].redevance_communale_des_mines_aurifere_kg!
-
-      return acc
-    }, {} as { [entrepriseId: string]: { [annee: string]: number | null } })
-  }
-
-  return null
-}
-
-export { redevanceCommunaleMinesAurifiereGet }
