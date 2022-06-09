677e03c0dc0901daafc7a87277ada7bf7aa3daba
vmaubert <v.maubert@code-troopers.com>
2022-06-09 16:20:54 +0200

    récupère les substances fiscales

--- packages/api/src/tools/demarches/tests-creation.ts
+++ packages/api/src/tools/demarches/tests-creation.ts
@@ -1,34 +1,48 @@
 import '../../init'
-import {titresGet} from "../../database/queries/titres";
-import {userSuper} from "../../database/user-super";
-import {SubstancesFiscales} from "camino-common/src/substance";
-import {titresActivitesGet} from "../../database/queries/titres-activites";
-
-
+import { userSuper } from '../../database/user-super'
+import { SubstancesFiscales } from 'camino-common/src/substance'
+import { titresActivitesGet } from '../../database/queries/titres-activites'
 
 const writeEtapesForTest = async () => {
+  const activites = await titresActivitesGet(
+    {
+      typesIds: ['grx', 'gra', 'wrp'],
+      statutsIds: ['dep'],
+      annees: [2021]
+    },
+    { fields: { titre: { substances: { legales: { id: {} } } } } },
+    userSuper
+  )
 
+  activites.forEach(activites => {
+    const substanceLegalesWithFiscales = (activites.titre?.substances ?? [])
+      .flatMap(s => s.legales)
+      .filter(s =>
+        SubstancesFiscales.some(
+          ({ substanceLegaleId }) => substanceLegaleId === s.id
+        )
+      )
 
-    const activites = await titresActivitesGet(
-        {
-            typesIds: ['grx', 'gra', 'wrp'],
-            statutsIds: ['dep'],
-            annees: [2021]
-        },
-        { fields: { titre: { substances: { legales: {id: {}} }}} },
-        userSuper
-    )
+    if (substanceLegalesWithFiscales.length > 1) {
+      console.error(
+        'BOOM, titre avec plusieurs substances ',
+        activites.titre?.id
+      )
+      console.error(activites.contenu)
 
-    activites.forEach((activites, i) => {
-        const substanceLegalesWithFiscales = (activites.titre?.substances ?? [])
-            .flatMap(s => s.legales)
-            .filter(s => SubstancesFiscales.some(({substanceLegaleId}) => substanceLegaleId === s.id))
-
-        if(substanceLegalesWithFiscales.length > 1){
-            console.error('BOOM, titre avec plusieurs substances ', activites.titre?.id, i)
-        }
-    })
-
+      substanceLegalesWithFiscales.forEach(s => {
+        SubstancesFiscales.some(
+          ({ substanceLegaleId }) => substanceLegaleId === s.id
+        )
+        console.error(
+          s.id,
+          SubstancesFiscales.filter(
+            ({ substanceLegaleId }) => substanceLegaleId === s.id
+          ).map(({ id }) => id)
+        )
+      })
+    }
+  })
 }
 
 writeEtapesForTest()
