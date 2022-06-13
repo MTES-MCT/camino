4e095b9e6cd4412eec48c2887d6409e86e319322
vmaubert <v.maubert@code-troopers.com>
2022-06-13 16:20:49 +0200

    fix(substances): conserve l’ordre renseigné sur les substances dans les étapes (#86)
    Co-authored-by: Bitard Michaël <bitard.michael@gmail.com>

---
+++ packages/api/src/knex/migrations-data/20220609145836_fix-substances-ordre.js
@@ -0,0 +1,54 @@
+const mostImportantSubstances = ['auru', 'nacl']
+const leastImportantSubstances = [
+  'tmas',
+  'suco',
+  'scor',
+  'oooo',
+  'rxxx',
+  'trxx'
+]
+
+exports.up = async knex => {
+  const titresEtapesSubstances = await knex('titres_substances')
+    .select('titre_etape_id')
+    .count('titre_etape_id')
+    .whereNull('ordre')
+    .groupBy('titre_etape_id')
+  for (let i = 0; i < titresEtapesSubstances.length; i++) {
+    const titreEtapeSubstances = titresEtapesSubstances[i]
+    if (titreEtapeSubstances.count !== '1') {
+      const entries = await knex('titres_substances').where(
+        'titre_etape_id',
+        titreEtapeSubstances.titreEtapeId
+      )
+      const sorted = entries.sort((entry1, entry2) => {
+        if (mostImportantSubstances.include(entry1.substanceId)) {
+          return -1
+        }
+        if (mostImportantSubstances.include(entry2.substanceId)) {
+          return 1
+        }
+
+        if (leastImportantSubstances.includes(entry1.substanceId)) {
+          return 1
+        }
+
+        if (leastImportantSubstances.includes(entry2.substanceId)) {
+          return -1
+        }
+
+        return 0
+      })
+
+      for (let j = 0; j < sorted.length; j++) {
+        const titreEtapeSubstance = sorted[j]
+        await knex('titres_substances')
+          .where('titre_etape_id', titreEtapeSubstance.titreEtapeId)
+          .andWhere('substanceId', titreEtapeSubstance.substanceId)
+          .update('ordre', j)
+      }
+    }
+  }
+}
+
+exports.down = () => ({})
