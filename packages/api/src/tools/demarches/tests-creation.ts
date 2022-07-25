4f983576fa4deff196f270a3bda095ff2ca7b94e
Bitard Michaël <bitard.michael@gmail.com>
2022-07-25 11:03:33 +0200

    extract demarche

--- packages/api/src/tools/demarches/tests-creation.ts
+++ packages/api/src/tools/demarches/tests-creation.ts
@@ -1,47 +1,79 @@
 import '../../init'
+
+import { titresDemarchesGet } from '../../database/queries/titres-demarches'
 import { userSuper } from '../../database/user-super'
-import { SubstancesFiscales } from 'camino-common/src/substance'
-import { titresActivitesGet } from '../../database/queries/titres-activites'
+import { titreDemarcheDepotDemandeDateFind } from '../../business/rules/titre-demarche-depot-demande-date-find'
+import { Etape } from '../../business/rules-demarches/arm/oct.machine'
+import { writeFileSync } from 'fs'
+import {
+  isEtapesOk,
+  toMachineEtape
+} from '../../business/rules-demarches/machine-helper'
 
 const writeEtapesForTest = async () => {
-  const activites = await titresActivitesGet(
+  const demarcheDefinition = {
+    titreTypeId: 'arm',
+    demarcheTypeIds: ['oct'],
+    dateDebut: '2019-10-31'
+  }
+  const demarches = await titresDemarchesGet(
     {
-      typesIds: ['grx', 'gra', 'wrp'],
-      statutsIds: ['dep']
+      titresTypesIds: [demarcheDefinition.titreTypeId.slice(0, 2)],
+      titresDomainesIds: [demarcheDefinition.titreTypeId.slice(2)],
+      typesIds: demarcheDefinition.demarcheTypeIds
     },
-    { fields: { titre: { substances: { legales: { id: {} } } } } },
+    {
+      fields: {
+        titre: { id: {}, demarches: { etapes: { id: {} } } },
+        etapes: { id: {} },
+        type: { etapesTypes: { etapesStatuts: { id: {} } } }
+      }
+    },
     userSuper
   )
 
-  activites.forEach(activites => {
-    const substanceLegalesWithFiscales = (activites.titre?.substances ?? [])
-      .flatMap(s => s.legales)
-      .filter(s =>
-        SubstancesFiscales.some(
-          ({ substanceLegaleId }) => substanceLegaleId === s.id
-        )
-      )
+  const toutesLesEtapes = demarches
+    .filter(demarche => demarche.etapes?.length)
+    .filter(demarche => {
+      const date = titreDemarcheDepotDemandeDateFind(demarche.etapes!)
 
-    if (substanceLegalesWithFiscales.length > 1) {
-      console.error(
-        'BOOM, titre avec plusieurs substances ',
-        activites.titre?.id
-      )
-      console.error(activites.contenu)
+      return (date ?? '') > demarcheDefinition.dateDebut
+    })
+    .map((demarche, index) => {
+      const etapes: Etape[] =
+        demarche?.etapes
+          ?.sort((a, b) => (a.ordre ?? 0) - (b.ordre ?? 0))
+          ?.map(etape => {
+            if (etape?.contenu?.arm) {
+              etape.contenu = { arm: etape.contenu?.arm }
+            } else {
+              delete etape.contenu
+            }
 
-      substanceLegalesWithFiscales.forEach(s => {
-        SubstancesFiscales.some(
-          ({ substanceLegaleId }) => substanceLegaleId === s.id
-        )
+            return toMachineEtape(etape)
+          }) ?? []
+      try {
+        if (!isEtapesOk(etapes)) {
+          console.warn(
+            `https://camino.beta.gouv.fr/titres/${demarche.titreId} => démarche "${demarche.typeId}"`
+          )
+        }
+      } catch (e) {
         console.error(
-          s.id,
-          SubstancesFiscales.filter(
-            ({ substanceLegaleId }) => substanceLegaleId === s.id
-          ).map(({ id }) => id)
+          `https://camino.beta.gouv.fr/titres/${demarche.titreId} => démarche "${demarche.typeId}"`
         )
+      }
+
+      const etapesAnonymes = etapes.map((etape, index) => {
+        return { ...etape, date: index.toString() }
       })
-    }
-  })
+
+      return { id: index, etapes: etapesAnonymes }
+    })
+  writeFileSync(
+    'src/business/rules-demarches/arm/oct.cas.json',
+    JSON.stringify(toutesLesEtapes)
+  )
 }
 
 writeEtapesForTest()
