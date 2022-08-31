198f02f8f2864da43e68b665bae0ae404ec097e9
Michaël Bitard <bitard.michael@gmail.com>
2022-08-31 11:03:36 +0200

    fix(daily): récupère la liste des fichiers présents sur le disque (#173)

--- packages/api/src/tools/documents/files-index-build.ts
+++ packages/api/src/tools/documents/files-index-build.ts
@@ -3,9 +3,7 @@ import { basename } from 'path'
 
 import { Index } from '../../types'
 
-export const filesIndexBuild = (
-  path = './packages/api/files'
-): Index<string> => {
+export const filesIndexBuild = (path = './files'): Index<string> => {
   const filesNames = execSync(`find ${path} | grep pdf`).toString().split('\n')
 
   return filesNames.reduce((res: Index<string>, fileName) => {
