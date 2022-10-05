e0ff8064906260524180bc17cb848f359745bdd0
Benjamin COUSIN <benou.cousin@gmail.com>
2022-10-05 11:49:10 +0200

    fix(footer): ne plus mettre de hauteur fixe sur le footer (#211)

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -3,7 +3,7 @@ import { app } from '@storybook/vue3'
 import { h } from 'vue'
 // This is a global mock for the router-link component in the storybooks. Without it, we had some nasty warnings
 // TODO 2022-03-23 fix router-lint warning in jest... with storybook-vue3-router once jest support esm
-app.component('router-link', h('div', { type: 'primary' }))
+app.component('router-link', h('a', { type: 'primary' }))
 export const parameters = {
   actions: { argTypesRegex: '^on.*' },
   controls: {
