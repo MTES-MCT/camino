932d5a878503ab26d2c61a2ff146cab4d38785a9
Bitard Michaël <bitard.michael@gmail.com>
2022-11-23 09:48:32 +0100

    remove make-dir

--- packages/api/src/dev/api/graphql-dsl-to-query-convert.js
+++ packages/api/src/dev/api/graphql-dsl-to-query-convert.js
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch'
 const { getIntrospectionQuery } = require('graphql')
 const fs = require('fs')
-const makeDir = require('make-dir')
 const url = `http://localhost:4000`
 
 const dir = 'docs-sources/api'
@@ -10,7 +9,7 @@ fs.rm(`./${dir}`, { recursive: true, force: true }, err => {
   if (err) {
     throw err
   }
-  makeDir(`./${dir}`)
+  fs.mkdirSync(`./${dir}`, { recursive: true })
 })
 
 fetch(url, {
