b87fec7c37ca31b3ef7ba214805dc702505cd39a
Michaël Bitard <bitard.michael@gmail.com>
2022-12-07 10:18:40 +0100

    fix(api): le daily et le monthly fonctionnent (#288)

--- packages/api/src/scripts/monthly.ts
+++ packages/api/src/scripts/monthly.ts
@@ -1,11 +1,49 @@
 import '../init.js'
+import { consoleOverride } from '../config/logger.js'
+import { mailjetSend } from '../tools/api-mailjet/emails.js'
+import { readFileSync, writeFileSync, createWriteStream } from 'fs'
+import * as Console from 'console'
 import { monthly } from '../business/monthly.js'
 
-monthly()
+const logFile = '/tmp/monthly.log'
+
+const output = createWriteStream(logFile)
+if (process.env.CAMINO_STAGE) {
+  const logger = new Console.Console({ stdout: output, stderr: output })
+  // eslint-disable-next-line no-console
+  console.log = logger.log
+  consoleOverride(false)
+}
+
+const tasks = async () => {
+  console.info('Tâches mensuelles : démarrage')
+  // Réinitialise les logs qui seront envoyés par email
+  writeFileSync(logFile, '')
+  try {
+    await monthly()
+  } catch (e) {
+    console.error('Erreur durant le monthly', e)
+  }
+
+  if (process.env.CAMINO_STAGE) {
+    const emailBody = `Résultats de ${process.env.ENV} \n${readFileSync(
+      logFile
+    ).toString()}`
+    await mailjetSend([process.env.ADMIN_EMAIL!], {
+      Subject: `[Camino][${process.env.ENV}] Résultats du monthly`,
+      'Text-part': emailBody
+    })
+  }
+  console.info('Tâches mensuelles : terminé')
+}
+
+tasks()
   .then(() => {
+    output.end()
     process.exit()
   })
   .catch(e => {
-    console.error('monthly en échec', e)
+    console.error('Erreur', e)
+    output.end()
     process.exit(1)
   })
