8cce3af106f18eea07e23205cab988970a373a11
Bitard Michaël <bitard.michael@gmail.com>
2022-12-08 14:37:43 +0100

    chore(api): rend les tests d'intégration parallèle

--- packages/api/vitest.integration.config.ts
+++ packages/api/vitest.integration.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['**/*.test.integration.ts'],
-    threads: false,
+    threads: true,
+    isolate: true,
     setupFiles: '../tests/vitestSetup.ts',
     testTimeout: 10000,
     hookTimeout: 30000
