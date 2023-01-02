a6b0ef04d47c7635b95dd21812e9ff260d46c6cc
Benjamin COUSIN <b.cousin@code-troopers.com>
2023-01-02 15:11:21 +0100

    fix(statistiques): calcul de la surface sur l'octroi (#306)

--- packages/api/src/api/rest/statistiques/evolution-titres.ts
+++ packages/api/src/api/rest/statistiques/evolution-titres.ts
@@ -97,20 +97,18 @@ export const evolutionTitres = async (
   const surface: {
     rows: { annee: CaminoAnnee; count: string }[]
   } = await knex.raw(`
-      select substring(et."date", 0, 5) as annee, sum(t_surface.surface * 100) as count from titres_etapes et 
-      join titres_demarches td on td.id  = et.titre_demarche_id 
+  select substring(tp."date_debut", 0, 5) as annee, sum(t_surface.surface * 100) as count from titres_phases tp
+      join titres_demarches td on td.id  = tp.titre_demarche_id  
       join titres t on t.id = td.titre_id 
       join titres_etapes t_surface on t_surface.id =  t.props_titre_etapes_ids ->> 'surface'
-      where et.type_id = '${ETAPES_TYPES.depotDeLaDemande}' 
-      and et.archive is false
-      and td.type_id in (${toJoinSQL(demarcheOctroiTypeIds)})
-      and t.type_id = '${titreTypeId}'
-      and substring(et."date", 0, 5)::int >= ${anneeDepart}
-      and exists (select * from titres_communes tc join communes c on c.id = tc.commune_id where tc.titre_etape_id = t.props_titre_etapes_ids ->> 'points' and c.departement_id::text in (${toJoinSQL(
-        departements
-      )}))
-      group by substring(et."date", 0, 5)
-      `)
+  where td.type_id in (${toJoinSQL(demarcheOctroiTypeIds)})
+  and t.type_id = '${titreTypeId}'
+  and substring(tp."date_debut", 0, 5)::int >= ${anneeDepart} 
+  and exists (select * from titres_communes tc join communes c on c.id = tc.commune_id where tc.titre_etape_id = t.props_titre_etapes_ids ->> 'points' and c.departement_id::text in (${toJoinSQL(
+    departements
+  )}))
+  group by substring(tp."date_debut", 0, 5)
+`)
 
   return {
     depot: { ...annee, ...toRecord(depot.rows) },
