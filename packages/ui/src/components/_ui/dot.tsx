dbc76f8730f0fb2dab027af6bce42eb9d85e05e8
Bitard Michaël <bitard.michael@gmail.com>
2023-01-03 11:39:50 +0100

    dot color is optional

--- packages/ui/src/components/_ui/dot.tsx
+++ packages/ui/src/components/_ui/dot.tsx
@@ -1,7 +1,7 @@
 import { Couleur } from 'camino-common/src/static/couleurs'
 
 export interface Props {
-  color: `bg-${Couleur}`
+  color?: `bg-${Couleur}`
 }
 export function Dot(props: Props) {
   return <span class={`${props.color ?? 'bg-neutral'} pill dot mr-xs`} />
