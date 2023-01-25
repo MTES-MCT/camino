bd4553e1562746066b1e030789035d75baaf21f0
Benjamin COUSIN <benou.cousin@gmail.com>
2023-01-25 11:45:38 +0100

    correction phase pex geothermie

--- packages/api/src/business/rules/titre-etape-publication-check.ts
+++ packages/api/src/business/rules/titre-etape-publication-check.ts
@@ -1,12 +1,31 @@
-interface IDemarchePublicationIndex {
-  [id: string]: string[]
-}
+import {
+  EtapeTypeId,
+  ETAPES_TYPES
+} from 'camino-common/src/static/etapesTypes.js'
+import { TitreTypeId } from 'camino-common/src/static/titresTypes.js'
 
-const demarcheEtapesTypesPublication = {
-  arm: ['def', 'sco', 'aco'],
-  axm: ['dex', 'rpu'],
-  prm: ['rpu']
-} as IDemarchePublicationIndex
+type SubTitreTypeId = Extract<TitreTypeId, 'arm' | 'axm' | 'prm' | 'pxg'>
+const demarcheEtapesTypesPublication: {
+  [key in SubTitreTypeId]: readonly EtapeTypeId[]
+} = {
+  arm: [
+    ETAPES_TYPES.decisionDeLOfficeNationalDesForets,
+    ETAPES_TYPES.signatureDeLautorisationDeRechercheMiniere,
+    ETAPES_TYPES.avenantALautorisationDeRechercheMiniere
+  ],
+  axm: [
+    ETAPES_TYPES.decisionDeLadministration,
+    ETAPES_TYPES.publicationDeDecisionAuRecueilDesActesAdministratifs
+  ],
+  prm: [ETAPES_TYPES.publicationDeDecisionAuRecueilDesActesAdministratifs],
+  pxg: [ETAPES_TYPES.decisionDeLadministration]
+} as const
+
+const isSubTitreTypeId = (
+  titreTypeId: TitreTypeId
+): titreTypeId is SubTitreTypeId => {
+  return Object.keys(demarcheEtapesTypesPublication).includes(titreTypeId)
+}
 
 /**
  * Vérifie si l'étape est une étape de publication
@@ -14,10 +33,15 @@ const demarcheEtapesTypesPublication = {
  * @param titreTypeId - id du type de titre
  */
 export const titreEtapePublicationCheck = (
-  etapeTypeId: string,
-  titreTypeId: string
+  etapeTypeId: EtapeTypeId,
+  titreTypeId: TitreTypeId
 ) =>
   !!(
-    ['dpu', 'dup', 'ihi'].includes(etapeTypeId) ||
-    demarcheEtapesTypesPublication[titreTypeId]?.includes(etapeTypeId)
+    [
+      ETAPES_TYPES.publicationDeDecisionAuJORF,
+      ETAPES_TYPES.publicationDeDecisionAdministrativeAuJORF,
+      ETAPES_TYPES.informationsHistoriquesIncompletes
+    ].includes(etapeTypeId) ||
+    (isSubTitreTypeId(titreTypeId) &&
+      demarcheEtapesTypesPublication[titreTypeId]?.includes(etapeTypeId))
   )
