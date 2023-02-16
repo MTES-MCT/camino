edfc618033ec3392ade02778189a38a85415502b
vmaubert <v.maubert@code-troopers.com>
2023-02-16 16:37:27 +0100

    fix build

--- packages/api/src/server/connected-catcher.ts
+++ packages/api/src/server/connected-catcher.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import { User } from 'camino-common/src/roles.js'
 import { constants } from 'http2'
+import { CaminoRestRoutes } from 'camino-common/src/rest.js'
 
 export const connectedCatcher = async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => {
-  if (!req.url.includes('/moi') && !req.url.includes('/config')) {
+  if (
+    !req.url.includes(CaminoRestRoutes.moi) &&
+    !req.url.includes(CaminoRestRoutes.config)
+  ) {
     if (req.cookies.shouldBeConnected) {
       const reqUser = req.user as User
       if (!reqUser) {
