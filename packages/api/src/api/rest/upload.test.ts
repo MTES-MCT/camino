557c19f156084307dac32cb876c66db80d5a0153
Bitard Michaël <bitard.michael@gmail.com>
2023-03-20 15:43:26 +0100

    fix test

--- packages/api/src/api/rest/upload.test.ts
+++ packages/api/src/api/rest/upload.test.ts
@@ -1,20 +1,11 @@
-import { dbManager } from '../../../tests/db-manager.js'
 import { ADMINISTRATION_IDS } from 'camino-common/src/static/administrations.js'
-import { vi, afterAll, beforeAll, describe, test, expect } from 'vitest'
+import { vi, describe, test, expect } from 'vitest'
 import { uploadAllowedMiddleware } from '../../server/upload.js'
 import { Request, Response } from 'express'
 import { TestUser } from 'camino-common/src/tests-utils.js'
 
 console.info = vi.fn()
 describe('téléversement de fichier par rest (tus)', () => {
-  beforeAll(async () => {
-    await dbManager.populateDb()
-  })
-
-  afterAll(async () => {
-    await dbManager.closeKnex()
-  })
-
   describe('permission de téléverser', () => {
     test.each<[TestUser, number]>([
       [
@@ -41,9 +32,9 @@ describe('téléversement de fichier par rest (tus)', () => {
       ],
       [{ role: 'entreprise', entreprises: [] }, 200],
       [{ role: 'defaut' }, 403],
-    ])('retourne le code $code pour un utilisateur "$user"', async (user, code) => {
+    ])('retourne pour un utilisateur %o le code $d', async (user, code) => {
       vi.resetAllMocks()
-      const req = { user } as unknown as Request
+      const req = { auth: user } as unknown as Request
       const res = { sendStatus: vi.fn() } as unknown as Response
       const next = vi.fn()
       await uploadAllowedMiddleware(req, res, next)
