4c64e7b2d257586f6cffb9edb75be0df70f38613
Bitard Michaël <bitard.michael@gmail.com>
2023-03-28 14:29:04 +0200

    feat(onboarding): un mail de bienvenue est envoyé automatiquement à la première connexion d'un utilisateur

--- packages/api/src/server/user-loader.ts
+++ packages/api/src/server/user-loader.ts
@@ -3,9 +3,10 @@ import { Request as JWTRequest } from 'express-jwt'
 import { knex } from '../knex.js'
 import { userIdGenerate } from '../api/graphql/resolvers/utilisateurs.js'
 import { userByEmailGet, utilisateurCreate } from '../database/queries/utilisateurs.js'
-import { emailsSend } from '../tools/api-mailjet/emails.js'
+import { emailsSend, emailsWithTemplateSend } from '../tools/api-mailjet/emails.js'
 import { formatUser } from '../types.js'
 import { getCurrent } from 'camino-common/src/date.js'
+import { EmailTemplateId } from '../tools/api-mailjet/types.js'
 
 export const userLoader = async (req: JWTRequest<{ email?: string; family_name?: string; given_name?: string }>, _res: express.Response, next: express.NextFunction) => {
   try {
@@ -32,11 +33,12 @@ export const userLoader = async (req: JWTRequest<{ email?: string; family_name?:
           { fields: { entreprises: { id: {} } } }
         )
 
-        emailsSend(
+        await emailsSend(
           [process.env.ADMIN_EMAIL!],
           `Nouvel utilisateur ${user.email} créé`,
           `L'utilisateur ${user.nom} ${user.prenom} vient de se créer un compte : ${process.env.OAUTH_URL}/utilisateurs/${user.id}`
         )
+        await emailsWithTemplateSend([reqUser.email], EmailTemplateId.CREATION_COMPTE, {})
       } else if (user.nom !== reqUser.family_name || user.prenom !== reqUser.given_name) {
         // mise à jour du nom et du prénom de l’utilisateur
         await knex('utilisateurs').update({ nom: reqUser.family_name, prenom: reqUser.given_name }).where('email', reqUser.email)
