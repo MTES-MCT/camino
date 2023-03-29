3832e1456b4a22dbe71eafa090f2cf5c4af76567
Michaël Bitard <bitard.michael@gmail.com>
2023-03-29 09:32:51 +0200

    feat(onboarding): un mail de bienvenue est envoyé automatiquement à la première connexion d'un utilisateur (#483)

--- packages/api/src/tools/api-mailjet/types.ts
+++ packages/api/src/tools/api-mailjet/types.ts
@@ -2,6 +2,7 @@ export enum EmailTemplateId {
   DEMARCHE_CONFIRMATION_DEPOT = 3413770,
   ACTIVITES_NOUVELLES = 3794302,
   ACTIVITES_RELANCE = 3785214,
+  CREATION_COMPTE = 4628355,
 }
 
 export const EmailAdministration = {
