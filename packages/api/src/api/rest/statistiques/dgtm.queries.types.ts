5f8aa99f35cf99317529b56ff8a5c3a09c95f14c
Bitard Michaël <bitard.michael@gmail.com>
2023-04-17 10:49:29 +0200

    fix error

--- packages/api/src/api/rest/statistiques/dgtm.queries.types.ts
+++ packages/api/src/api/rest/statistiques/dgtm.queries.types.ts
@@ -2,17 +2,18 @@
 
 /** 'GetProductionOr' parameters type */
 export interface IGetProductionOrParams {
-  substance?: string | null | void
+  substance?: string | null | void;
 }
+
 
 /** 'GetProductionOr' return type */
 export interface IGetProductionOrResult {
-  annee: number
-  count: string | null
+  annee: number;
+  count: string | null;
 }
 
 /** 'GetProductionOr' query type */
 export interface IGetProductionOrQuery {
-  params: IGetProductionOrParams
-  result: IGetProductionOrResult
+  params: IGetProductionOrParams;
+  result: IGetProductionOrResult;
 }
