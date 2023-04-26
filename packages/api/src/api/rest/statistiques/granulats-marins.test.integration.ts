005f80a132d80d6953a089ff961567425183b13e
Bitard Michaël <bitard.michael@gmail.com>
2023-04-26 18:01:29 +0200

    add integration tests

---
+++ packages/api/src/api/rest/statistiques/granulats-marins.test.integration.ts
@@ -0,0 +1,25 @@
+import { restCall } from '../../../../tests/_utils/index.js'
+import { dbManager } from '../../../../tests/db-manager.js'
+import { expect, test, afterAll, beforeAll, vi } from 'vitest'
+import { CaminoRestRoutes } from 'camino-common/src/rest.js'
+import type { Pool } from 'pg'
+
+console.info = vi.fn()
+console.error = vi.fn()
+let dbPool: Pool
+
+beforeAll(async () => {
+  const { pool } = await dbManager.populateDb()
+  dbPool = pool
+})
+
+afterAll(async () => {
+  await dbManager.closeKnex()
+})
+
+test('peut récupérer les statistiques des granulats marins', async () => {
+  const tested = await restCall(dbPool, CaminoRestRoutes.statistiquesGranulatsMarins, {}, undefined)
+
+  expect(tested.statusCode).toBe(200)
+  expect(tested.body).toMatchSnapshot()
+})
