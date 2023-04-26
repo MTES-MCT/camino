14e432cdd55cff7444b4adeed01758d52ebab22c
Bitard Michaël <bitard.michael@gmail.com>
2023-04-26 15:49:10 +0200

    set not null

--- packages/api/src/knex/migrations/20230425155734_titre-slug-not-nullable.ts
+++ packages/api/src/knex/migrations/20230425155734_titre-slug-not-nullable.ts
@@ -2,6 +2,8 @@ import { Knex } from 'knex'
 
 export const up = async (knex: Knex) => {
   await knex.raw('alter table titres alter column slug set not null;')
+  await knex.raw('ALTER TABLE titres_activites ALTER COLUMN "date" SET NOT NULL;')
+  await knex.raw('ALTER TABLE public.titres_activites ALTER COLUMN annee SET NOT NULL;')
 }
 
 export const down = () => ({})
