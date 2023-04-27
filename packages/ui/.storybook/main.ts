aea53c0938fdba6c794f241a2a8ac4526f6fba5b
Bitard Michaël <bitard.michael@gmail.com>
2023-04-27 16:46:29 +0200

    remove mdx support

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/vue3-vite'
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-styling'],
   framework: {
     name: '@storybook/vue3-vite',
