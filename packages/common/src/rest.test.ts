eb9c20e273ad589625c5426cffafa25c442ffcd4
Michaël Bitard <bitard.michael@gmail.com>
2023-05-09 09:41:20 +0200

    fix(api): conserve les routes existantes (#545)

--- packages/common/src/rest.test.ts
+++ packages/common/src/rest.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from 'vitest'
 import { CaminoRestRoutes, getRestRoute } from './rest'
 
 test('getRestRoute', () => {
-  expect(getRestRoute(CaminoRestRoutes.newsletter, { id: 'userId' })).toBe('/utilisateurs/userId/newsletter')
+  expect(getRestRoute(CaminoRestRoutes.newsletter, { id: 'userId' })).toBe('/rest/utilisateurs/userId/newsletter')
 })
