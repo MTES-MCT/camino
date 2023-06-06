21a5c0ecb4b0029ecd9f181e1f5f54b4eac968c8
vmaubert <v.maubert@code-troopers.com>
2023-06-06 17:45:48 +0200

    fix test

--- packages/common/src/rest.test.ts
+++ packages/common/src/rest.test.ts
@@ -3,5 +3,6 @@ import { getRestRoute } from './rest'
 
 test('getRestRoute', () => {
   expect(getRestRoute('/rest/utilisateurs/:id/newsletter', { id: 'userId' })).toBe('/rest/utilisateurs/userId/newsletter')
-  expect(getRestRoute('/rest/utilisateurs/:id/newsletter', { id: 'userId' }, { toto: ['plop', 'plip'] })).toBe('/rest/utilisateurs/userId/newsletter?toto[]=plop&toto[]=plip')
+  expect(getRestRoute('/rest/utilisateurs/:id/newsletter', { id: 'userId' }, { toto: ['plop', 'plip'] })).toBe('/rest/utilisateurs/userId/newsletter?toto%5B%5D=plop&toto%5B%5D=plip')
+  expect(getRestRoute('/rest/utilisateurs/:id/newsletter', { id: 'userId' }, { toto: 'plop' })).toBe('/rest/utilisateurs/userId/newsletter?toto=plop')
 })
