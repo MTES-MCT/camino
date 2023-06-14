975da96c9c69a4b933b0bdda0e0a29ed40d59d4a
Bitard Michaël <bitard.michael@gmail.com>
2023-06-14 11:27:02 +0200

    fix(ui): les dashboards d'entreprises fonctionnent à nouveau

--- packages/ui/src/components/dashboard.tsx
+++ packages/ui/src/components/dashboard.tsx
@@ -17,28 +17,29 @@ export const Dashboard = defineComponent({
     const user = store.state.user.element
 
     const hasEntreprises: boolean = store.getters['user/hasEntreprises']
-
     const isONF: boolean = store.getters['user/isONF']
     const isPTMG: boolean = store.getters['user/isPTMG']
     const isDREAL: boolean = store.getters['user/isDREAL']
     const isDGTM: boolean = store.getters['user/isDGTM']
     const entreprises = store.getters['user/user']?.entreprises ?? []
-    if (!isONF && !isPTMG && !isDREAL && !isDGTM) {
+
+    if (!hasEntreprises && !isONF && !isPTMG && !isDREAL && !isDGTM) {
       store.commit('titres/reset')
       store.dispatch('titres/init')
       router.replace({ name: 'titres' })
+    } else {
+      let dashboard = <div>Loading</div>
+      if (hasEntreprises) {
+        dashboard = <PureEntrepriseDashboard apiClient={dashboardApiClient} user={user} entreprises={entreprises} displayActivites={canReadActivites(user)} />
+      } else if (isONF) {
+        dashboard = <PureONFDashboard apiClient={dashboardApiClient} />
+      } else if (isPTMG) {
+        dashboard = <PurePTMGDashboard apiClient={dashboardApiClient} />
+      } else if (isDREAL || isDGTM) {
+        dashboard = <PureDrealDashboard apiClient={dashboardApiClient} isDGTM={isDGTM} />
+      }
+      return () => <dashboard />
     }
-
-    let dashboard = <div>Loading</div>
-    if (hasEntreprises) {
-      dashboard = <PureEntrepriseDashboard apiClient={dashboardApiClient} user={user} entreprises={entreprises} displayActivites={canReadActivites(user)} />
-    } else if (isONF) {
-      dashboard = <PureONFDashboard apiClient={dashboardApiClient} />
-    } else if (isPTMG) {
-      dashboard = <PurePTMGDashboard apiClient={dashboardApiClient} />
-    } else if (isDREAL || isDGTM) {
-      dashboard = <PureDrealDashboard apiClient={dashboardApiClient} isDGTM={isDGTM} />
-    }
-    return () => <dashboard />
+    return () => null
   },
 })
