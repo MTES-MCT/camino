38e024cc50e029523f5fe27af5f8e1939b9d05e5
Bitard Michaël <bitard.michael@gmail.com>
2023-06-20 15:20:33 +0200

    premier test brut

--- packages/api/src/scripts/jorf-scan.ts
+++ packages/api/src/scripts/jorf-scan.ts
@@ -29,34 +29,68 @@ const getLegiFranceToken = async (): Promise<Bearer> => {
     },
   })
 
+  if (response.ok) {
+
+  
   const responseBody = await response.json() as {access_token: string, token_type: 'Bearer', expires_in: 3600, scope: 'openid resource.READ'}
 
 
   return responseBody.access_token as Bearer
+  } else {
+    console.log('error', await response.text())
+    throw new Error('Pas de token')
+  }
 }
 
 export type Bearer = string & {__caminoType: 'Bearer'}
 export const getAllJorfFromDatabase = async (): Promise<void> => {
   // const token = await getLegiFranceToken()
   // console.log('token', token)
-  const token = 'XQGlnu55fSQYEGt7TONpAgnZt2MNvQQMTWoxxfEbWiycDApcuz76PS' as Bearer
+  const token = 'IxHucnguRPMSTsWtcl9LrWzOn682k0OKI230hkQHnxkEuT8GaBhANu' as Bearer
 
   const documentsFromDatabase = await getAllJorfDocuments(pool)
   console.log('documents JORF: ' + documentsFromDatabase.length)
 
   let ok = 0
+  let strange = 0
+  let error = 0
+  let not_found = 0
+  let multiple = 0
+  let differente = 0
   for (let index = 0; index < documentsFromDatabase.length; index++) {
     const document = documentsFromDatabase[index]
     const result = await searchPublication(token, document.titre_nom, document.date)
-    if (result) {
-      if (result.jorf !== document.jorf || result.nor !== document.nor) {
-        console.warn(`Publication différente trouvée pour le titre ${document.titre_nom} autour de la date ${document.date}`)
-      }
-      ok++
+    switch (result) {
+      case 'strange': 
+      strange++
+      break
+      case 'error':
+        error++
+        break
+      case 'not_found':
+        not_found++
+        break
+      case 'multiple_found':
+        multiple++
+        break
+      default:
+        if (result.jorf !== document.jorf || result.nor !== document.nor) {
+          console.warn(`Publication différente trouvée pour le titre ${document.titre_nom} autour de la date ${document.date}`)
+          differente++
+        } else {
+
+          ok++
+        }
+
     }
   }
-  console.log(`trouvé ${ok} parmi ${documentsFromDatabase.length}`)
-  // await searchPublication(token, "Juan Nova Est", toCaminoDate('2008-12-30'))
+  console.log(`trouvé ${ok}/${documentsFromDatabase.length}`)
+  console.log(`error ${error}/${documentsFromDatabase.length}`)
+  console.log(`not_found ${not_found}/${documentsFromDatabase.length}`)
+  console.log(`multiple ${multiple}/${documentsFromDatabase.length}`)
+  console.log(`strange ${strange}/${documentsFromDatabase.length}`)
+  console.log(`differente ${differente}/${documentsFromDatabase.length}`)
+  // await searchPublication(token, "Juan-de-Nova Est", toCaminoDate('2008-12-30'))
 }
 
 interface EntryResultTitre {
@@ -76,7 +110,7 @@ interface Publication {
   jorf: string
 }
 
-const searchPublication = async (bearer: Bearer, titre: string, date: CaminoDate): Promise<Publication | null> => {
+const searchPublication = async (bearer: Bearer, titre: string, date: CaminoDate): Promise<Publication | 'not_found' | 'multiple_found' | 'strange' | 'error'> => {
   const oneMonthBefore = dateAddMonths(date, -1)
   const oneMonthAfter = dateAddMonths(date, 1)
 
@@ -137,26 +171,27 @@ const searchPublication = async (bearer: Bearer, titre: string, date: CaminoDate
 
     const body = await response.json() as Result
 
+    // console.log(body)
     if (body.results.length === 0) {
       console.warn(`pas de publication trouvée pour titre ${titre} autour de la date ${date}`)
-      return null
+      return 'not_found'
     }
-    if (body.results.length > 0) {
+    if (body.results.length > 1) {
       console.warn(`plusieurs publications trouvées pour le titre ${titre} autour de la date ${date}`)
-      return null
+      return 'multiple_found'
     }
     if (body.results[0].titles.length !== 1) {
       console.warn(`plusieurs titres trouvées pour la publication du titre ${titre} autour de la date ${date}`)
+      return 'strange'
     }
     
-    console.log('body', body)
     return { nor: body.results[0].nor,
         jorf: body.results[0].titles[0].cid
     }
   } else {
     console.error(response.status)
     console.error(await response.text())
-    return null
+    return 'error'
   }
 }
 
