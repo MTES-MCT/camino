95df1b87b1573ff0afc18d84618796e929ee579a
Bitard Michaël <bitard.michael@gmail.com>
2023-06-22 15:43:18 +0200

    extract chartjs from main bundle

--- packages/api/src/api/graphql/resolvers/journaux.ts
+++ packages/api/src/api/graphql/resolvers/journaux.ts
@@ -8,13 +8,13 @@ import { Journaux, JournauxQueryParams } from 'camino-common/src/journaux.js'
 export const journaux = async (params: JournauxQueryParams, { user }: Context, info: GraphQLResolveInfo): Promise<Journaux> => {
   try {
     if (!canReadJournaux(user)) {
-      return { elements: [], page: 1, intervalle: 0, total: 0 }
+      return { elements: [], page: 1, intervalle: 10, total: 0 }
     }
     const fields = fieldsBuild(info)
 
     const { results, total } = await journauxGet(params, { fields: fields.elements }, user)
 
-    if (!results.length) return { elements: [], page: 1, intervalle: 0, total: 0 }
+    if (!results.length) return { elements: [], page: 1, intervalle: 10, total: 0 }
 
     return {
       elements: results,
