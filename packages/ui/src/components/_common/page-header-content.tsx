c082d12b47e847ccecc01ffe66c9af3d4aacbf3a
vmaubert <v.maubert@code-troopers.com>
2023-09-29 11:02:16 +0200

    fix(style): corrige l’alignement du bouton de création de demande

--- packages/ui/src/components/_common/page-header-content.tsx
+++ packages/ui/src/components/_common/page-header-content.tsx
@@ -16,7 +16,7 @@ export const PageContentHeader: FunctionalComponent<Props> = props => {
         <h1>{capitalize(props.nom)}</h1>
       </div>
 
-      <div class="fr-col-12 fr-col-md-6" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
+      <div class="fr-col-12 fr-col-md-6" style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-end' }}>
         {props.download ? <Downloads {...props.download} class="" /> : null}
         {props.renderButton !== null ? props.renderButton() : null}
       </div>
