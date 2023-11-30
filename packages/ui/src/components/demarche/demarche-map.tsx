a3040615ab68b5a42f1f62bb84a389790f63c701
Bitard Michaël <bitard.michael@gmail.com>
2023-11-30 17:16:47 +0100

    working with glyphs and points

--- packages/ui/src/components/demarche/demarche-map.tsx
+++ packages/ui/src/components/demarche/demarche-map.tsx
@@ -1,21 +1,22 @@
 import { FunctionalComponent, HTMLAttributes, defineComponent, onMounted, ref, Ref, computed } from 'vue'
 import { FullscreenControl, Map, NavigationControl, StyleSpecification, LayerSpecification, LngLatBounds, SourceSpecification } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
-import {z} from 'zod'
+import { z } from 'zod'
 import { DsfrSeparator } from '../_ui/dsfr-separator'
 import { FeatureMultiPolygon } from 'camino-common/src/demarche'
+import { random } from '@/utils/vue-tsx-utils'
 
 
 const contourLayerName = 'ContoursLine'
-type Props = {geojsonMultiPolygon: FeatureMultiPolygon}
+type Props = { geojsonMultiPolygon: FeatureMultiPolygon }
 
-    const baseMapNames = {
-      'OSM / fr': 'osm',
-      'OSM / hot': 'hot',
-      'Géoportail / Plan IGN': 'geoIGN',
-      'Géoportail / Photographies aériennes': 'geoAer',
-      'BRGM / Cartes géologiques 1/50 000': 'BRGMGeo',
-    } as const satisfies Record<string, LayerId>
+const baseMapNames = {
+  'OSM / fr': 'osm',
+  'OSM / hot': 'hot',
+  'Géoportail / Plan IGN': 'geoIGN',
+  'Géoportail / Photographies aériennes': 'geoAer',
+  'BRGM / Cartes géologiques 1/50 000': 'BRGMGeo',
+} as const satisfies Record<string, LayerId>
 
 const layers = ['osm', 'hot', 'geoIGN', 'geoAer', 'BRGMGeo'] as const
 const layerIdValidator = z.enum(layers)
@@ -23,10 +24,9 @@ type LayerId = z.infer<typeof layerIdValidator>
 
 const sdomOverlayName = 'SDOM (schéma départemental d’orientation minière)'
 
-//FIXME points
-//FIXME plusieurs cartes sur la même page
 //FIXME les titres voisins
 //FIXME Pierre-Olivier souhaite que les points s’affichent à partir d’un niveau de zoom
+//FIXME si on enlève les points et qu'on les remet, ils sont en dessous des contours...
 
 
 const overlayLayers = ['SDOM', 'Facades', 'RedevanceArcheologiePreventive', contourLayerName, 'ContoursFill', 'ContoursPoints', 'ContoursPointLabels'] as const
@@ -77,21 +77,23 @@ const layersSourceSpecifications: Record<LayerId, SourceSpecification> = {
     maxzoom: 16
   },
 }
-const staticOverlayLayersSourceSpecification: Record<Exclude<OverlayLayerId, 'ContoursLine' | 'ContoursFill' | 'ContoursPoints' | 'ContoursPointLabels'>, SourceSpecification> = {SDOM: {
-  type: 'raster',
-  tiles: ['https://datacarto.geoguyane.fr/wms/SDOM_GUYANE?service=WMS&request=GetMap&layers=ZONE2activiteminiereautoriseesouscontrainte%2CZONE1activiteminiereinterditesaufexploitationsouterraineetrecherchesaeriennes%2CZONE0activiteminiereinterdite%2CZone0potentielle&styles=&format=image/png&transparent=false&version=1.1.1&width=256&height=256&srs=EPSG%3A3857&bbox={bbox-epsg-3857}'],
-  attribution: 'GéoGuyane'
-},
-Facades: {
-  type: 'raster',
-  tiles: ['https://gisdata.cerema.fr/arcgis/services/Carte_vocation_dsf_2020/MapServer/WMSServer?service=WMS&request=GetMap&layers=0&styles=&format=image/png&transparent=true&version=1.1.1&width=256&height=256&srs=EPSG:3857&bbox={bbox-epsg-3857}'],
-  attribution: 'cerema'
-},
-RedevanceArcheologiePreventive: {
-  type: 'raster',
-  tiles: ['https://services.data.shom.fr/INSPIRE/wmts?layer=RAP_PYR_PNG_3857_WMTS&style=normal&tilematrixset=3857&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/png&TileMatrix={z}&TileCol={x}&TileRow={y}'],
-  attribution: 'shom'
-}}
+const staticOverlayLayersSourceSpecification: Record<Exclude<OverlayLayerId, 'ContoursLine' | 'ContoursFill' | 'ContoursPoints' | 'ContoursPointLabels'>, SourceSpecification> = {
+  SDOM: {
+    type: 'raster',
+    tiles: ['https://datacarto.geoguyane.fr/wms/SDOM_GUYANE?service=WMS&request=GetMap&layers=ZONE2activiteminiereautoriseesouscontrainte%2CZONE1activiteminiereinterditesaufexploitationsouterraineetrecherchesaeriennes%2CZONE0activiteminiereinterdite%2CZone0potentielle&styles=&format=image/png&transparent=false&version=1.1.1&width=256&height=256&srs=EPSG%3A3857&bbox={bbox-epsg-3857}'],
+    attribution: 'GéoGuyane'
+  },
+  Facades: {
+    type: 'raster',
+    tiles: ['https://gisdata.cerema.fr/arcgis/services/Carte_vocation_dsf_2020/MapServer/WMSServer?service=WMS&request=GetMap&layers=0&styles=&format=image/png&transparent=true&version=1.1.1&width=256&height=256&srs=EPSG:3857&bbox={bbox-epsg-3857}'],
+    attribution: 'cerema'
+  },
+  RedevanceArcheologiePreventive: {
+    type: 'raster',
+    tiles: ['https://services.data.shom.fr/INSPIRE/wmts?layer=RAP_PYR_PNG_3857_WMTS&style=normal&tilematrixset=3857&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image/png&TileMatrix={z}&TileCol={x}&TileRow={y}'],
+    attribution: 'shom'
+  }
+}
 
 
 const overlayConfigs: Record<OverlayLayerId, LayerSpecification> = {
@@ -111,95 +113,98 @@ const overlayConfigs: Record<OverlayLayerId, LayerSpecification> = {
     source: 'SDOM',
   },
   [contourLayerName]: {
-  id: contourLayerName,
-  type: 'line',
-  source: 'Contours',
+    id: contourLayerName,
+    type: 'line',
+    source: 'Contours',
 
-  paint: {
-    "line-color": '#000091',
-    "line-width": 2
+    paint: {
+      "line-color": '#000091',
+      "line-width": 2
+    }
+  },
+
+  ContoursFill: {
+    id: 'ContoursFill',
+    type: 'fill',
+    source: 'Contours',
+
+    paint: {
+      "fill-color": '#000091',
+      "fill-opacity": 0.2
+    }
+  },
+  ContoursPoints: {
+    id: 'ContoursPoints',
+    type: 'circle',
+    source: 'Points',
+
+    paint: {
+      'circle-color': '#000091',
+      "circle-radius": 10,
+    }
+  },
+  ContoursPointLabels: {
+    id: 'ContoursPointLabels',
+    'type': 'symbol',
+    'source': 'Points',
+    paint: {
+      'text-color': '#ffffff'
+    },
+    'layout': {
+        'text-field': ['get', 'plop'],
+        'text-allow-overlap': false,
+        'text-font': [
+            'Open Sans Semibold',
+            'Arial Unicode MS Bold'
+        ],
+    }
   }
-},
-
-ContoursFill: {
-  id: 'ContoursFill',
-  type: 'fill',
-  source: 'Contours',
-
-  paint: {
-    "fill-color": '#000091',
-    "fill-opacity": 0.2
-  }
-},
-ContoursPoints: {
-  id: 'ContoursPoints',
-  type: 'circle',
-  source: 'Points',
-
-  paint: {
-    'circle-color': '#000091',
-    "circle-radius": 10,
-  }
-},
-ContoursPointLabels: {
-  id: 'ContoursPointLabels',
-  type: 'symbol',
-  source: 'Points',
-  layout: {
-    // get the year from the source's "year" property
-    'text-field': ['get', 'point'],
-    'text-font': [
-        'Open Sans Semibold',
-        'Arial Unicode MS Bold'
-    ],
-    'text-offset': [0, 1.25],
-    'text-anchor': 'top'
 }
-
-}}
 
 export const DemarcheMap = defineComponent<Props>(props => {
 
+  const mapId = `map${random()}`
   const layersControlVisible = ref<boolean>(false)
   const map = ref<Map | null>(null) as Ref<Map | null>
-
 
   const points = computed(() => {
     const currentPoints = []
     let index = 0
     props.geojsonMultiPolygon.geometry.coordinates.forEach((topLevel, topLevelIndex) =>
-  topLevel.forEach((secondLevel, secondLevelIndex) =>
-    secondLevel.forEach(([y, x], currentLevelIndex) => {
-      // On ne rajoute pas le dernier point qui est égal au premier du contour...
-      if (props.geojsonMultiPolygon.geometry.coordinates[topLevelIndex][secondLevelIndex].length !== currentLevelIndex + 1) {
-        currentPoints.push({
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [y, x]
-          },
-          properties: {
-            point: index
+      topLevel.forEach((secondLevel, secondLevelIndex) =>
+        secondLevel.forEach(([y, x], currentLevelIndex) => {
+          // On ne rajoute pas le dernier point qui est égal au premier du contour...
+          if (props.geojsonMultiPolygon.geometry.coordinates[topLevelIndex][secondLevelIndex].length !== currentLevelIndex + 1) {
+            currentPoints.push({
+              type: 'Feature',
+              geometry: {
+                type: 'Point',
+                coordinates: [y, x]
+              },
+              properties: {
+                plop: `${index}`
+              }
+
+            })
+            index++
           }
-
         })
-        index++
-      }
-    })
-  )
-)
+      )
+    )
 
-return {
-  'type': 'FeatureCollection',
-  'features': currentPoints}
-}
-)
+    const value = {
+      'type': 'FeatureCollection',
+      'features': currentPoints
+    }
+    return value
+  }
+  )
 
 
   onMounted(() => {
     const style: StyleSpecification = {
       version: 8,
-      "glyphs": "mapbox://fonts/mapbox/{fontstack}/{range}.pbf",
+      "glyphs": "https://api.maptiler.com/fonts/{fontstack}/{range}.pbf?key=aYTJujaHzJbKYBWB7tcI",
       sources: {
         ...layersSourceSpecifications,
         ...staticOverlayLayersSourceSpecification,
@@ -227,7 +232,7 @@ return {
     }
 
     const mapLibre: Map = new Map({
-      container: 'map',
+      container: mapId,
       style,
       center: [2.2644635, 48.8588254],
       zoom: 6,
@@ -283,25 +288,25 @@ return {
         source: layer
       })
 
-      overlayLayers.forEach(overlayLayer => {
-        if (map.value?.getLayer(overlayLayer)) {
-          map.value?.moveLayer(layer, overlayLayer)
-        }
-      })
+    overlayLayers.forEach(overlayLayer => {
+      if (map.value?.getLayer(overlayLayer)) {
+        map.value?.moveLayer(layer, overlayLayer)
+      }
+    })
   }
 
 
   const toggleOverlayLayer = (overlayLayers: Readonly<OverlayLayerId[]>) => {
 
     overlayLayers.forEach(overlayLayer => {
-    if (map.value?.getLayer(overlayLayer)) {
-      map.value?.removeLayer(overlayLayer)
-    }else{
-      map.value?.addLayer(overlayConfigs[overlayLayer])
-    }
+      if (map.value?.getLayer(overlayLayer)) {
+        map.value?.removeLayer(overlayLayer)
+      } else {
+        map.value?.addLayer(overlayConfigs[overlayLayer])
+      }
 
-    map.value?.moveLayer(overlayLayer, contourLayerName)
-  })
+      map.value?.moveLayer(overlayLayer, contourLayerName)
+    })
 
   }
 
@@ -309,18 +314,18 @@ return {
     layersControlVisible.value = false
   }
 
-  return () => <><div id="map" style={{minHeight: '400px'}}>
+  return () => <><div id={mapId} style={{ minHeight: '400px' }}>
     <LayersControl
-    style={{display: layersControlVisible.value ? 'block' : 'none', zIndex: 3}}
-    onMouseleave={layerControlOnMouseleave}
-    setLayer={selectLayer}
-    toggleOverlayLayer={toggleOverlayLayer}
+      style={{ display: layersControlVisible.value ? 'block' : 'none', zIndex: 3 }}
+      onMouseleave={layerControlOnMouseleave}
+      setLayer={selectLayer}
+      toggleOverlayLayer={toggleOverlayLayer}
     />
-    </div></>
+  </div></>
 })
 
 
-const LayersControl: FunctionalComponent<{onMouseleave: HTMLAttributes['onMouseleave'], style: HTMLAttributes['style'], setLayer: (layer: LayerId) => void, toggleOverlayLayer: (overlay: Readonly<OverlayLayerId[]>) => void}> = (props) => {
+const LayersControl: FunctionalComponent<{ onMouseleave: HTMLAttributes['onMouseleave'], style: HTMLAttributes['style'], setLayer: (layer: LayerId) => void, toggleOverlayLayer: (overlay: Readonly<OverlayLayerId[]>) => void }> = (props) => {
 
   const selectLayer = (layer: LayerId) => {
     return () => props.setLayer(layer)
@@ -334,28 +339,28 @@ const LayersControl: FunctionalComponent<{onMouseleave: HTMLAttributes['onMousel
   const defaultOverlayLayer: OverlayName[] = ['Contours', 'Points']
 
   return <div class='maplibregl-ctrl-top-right'>
-    <div class='maplibregl-ctrl maplibregl-ctrl-group fr-p-2w' style={{zIndex: 3}}>
-        {
-          Object.entries(baseMapNames).map(([name, layer]) => <div key={layer} class="fr-fieldset__element fr-mb-1v">
-              <div class="fr-radio-group fr-radio-group--sm">
-                  <input type="radio" id={`radio-hint-${layer}`} name="radio-layers" onClick={selectLayer(layer)} checked={layer === defaultLayer}/>
-                  <label class="fr-label" for={`radio-hint-${layer}`}>
-                      {name}
-                  </label>
-              </div>
-          </div>)
-        }
-        <DsfrSeparator/>
-        {
-          overlayNames.map((name, index) => <div key={index} class="fr-fieldset__element fr-mb-1v">
-              <div class="fr-checkbox-group fr-checkbox-group--sm">
-                  <input type="checkbox" id={`checkbox-hint-${index}`} onClick={toggleOverlayLayer(name)} checked={defaultOverlayLayer.includes(name)}/>
-                  <label class="fr-label" for={`checkbox-hint-${index}`}>
-                      {name}
-                  </label>
-              </div>
-          </div>)
-        }
+    <div class='maplibregl-ctrl maplibregl-ctrl-group fr-p-2w' style={{ zIndex: 3 }}>
+      {
+        Object.entries(baseMapNames).map(([name, layer]) => <div key={layer} class="fr-fieldset__element fr-mb-1v">
+          <div class="fr-radio-group fr-radio-group--sm">
+            <input type="radio" id={`radio-hint-${layer}`} name="radio-layers" onClick={selectLayer(layer)} checked={layer === defaultLayer} />
+            <label class="fr-label" for={`radio-hint-${layer}`}>
+              {name}
+            </label>
+          </div>
+        </div>)
+      }
+      <DsfrSeparator />
+      {
+        overlayNames.map((name, index) => <div key={index} class="fr-fieldset__element fr-mb-1v">
+          <div class="fr-checkbox-group fr-checkbox-group--sm">
+            <input type="checkbox" id={`checkbox-hint-${index}`} onClick={toggleOverlayLayer(name)} checked={defaultOverlayLayer.includes(name)} />
+            <label class="fr-label" for={`checkbox-hint-${index}`}>
+              {name}
+            </label>
+          </div>
+        </div>)
+      }
     </div>
   </div>
 }
