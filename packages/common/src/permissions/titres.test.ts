7d3adbd37c184085912a6447586fedefd4a93a38
Bitard Michaël <bitard.michael@gmail.com>
2023-12-28 09:50:10 +0100

    reduce snapshot size by displaying only the true values

--- packages/common/src/permissions/titres.test.ts
+++ packages/common/src/permissions/titres.test.ts
@@ -14,9 +14,12 @@ test('getTitreFromTypeId pas de fusions', () => {
     [key in TitreTypeId]?: {
       count: 'single' | 'multiple'
       typeId: TitreTypeId
-    } | null
+    }
   }>((acc, titreTypeId) => {
-    acc[titreTypeId] = getLinkConfig(titreTypeId, [])
+    const linkConfig = getLinkConfig(titreTypeId, [])
+    if (linkConfig !== null) {
+      acc[titreTypeId] = linkConfig
+    }
 
     return acc
   }, {})
@@ -27,9 +30,12 @@ test('getTitreFromTypeId fusions', () => {
     [key in TitreTypeId]?: {
       count: 'single' | 'multiple'
       typeId: TitreTypeId
-    } | null
+    }
   }>((acc, titreTypeId) => {
-    acc[titreTypeId] = getLinkConfig(titreTypeId, [{ demarche_type_id: 'fus' }])
+    const fusion = getLinkConfig(titreTypeId, [{ demarche_type_id: 'fus' }])
+    if (fusion !== null) {
+      acc[titreTypeId] = fusion
+    }
 
     return acc
   }, {})
@@ -46,15 +52,20 @@ test.each<[TestUser, AdministrationId[], boolean]>([
 })
 
 describe('canCreateTitre', () => {
-  test.each<TitreTypeId>(TitresTypesIds)('vérifie si une entreprise peut créer un titre de type %p', titreTypeId => {
+  test('vérifie si une entreprise peut créer un titre de type', () => {
+    const result: { [key in TitreTypeId]?: true } = {}
+
     const user: User = { role: 'entreprise', entreprises: [], ...testBlankUser }
-    const actual = canCreateTitre(user, titreTypeId)
-    expect(actual).toMatchSnapshot()
-    if (actual) {
-      assertsCanCreateTitre(user, titreTypeId)
-    } else {
-      expect(() => assertsCanCreateTitre(user, titreTypeId)).toThrowError('permissions insuffisantes')
+    for (const titreTypeid of TitresTypesIds) {
+      const itCan = canCreateTitre(user, titreTypeid)
+      if (itCan) {
+        result[titreTypeid] = itCan
+        assertsCanCreateTitre(user, titreTypeid)
+      } else {
+        expect(() => assertsCanCreateTitre(user, titreTypeid)).toThrowError('permissions insuffisantes')
+      }
     }
+    expect(result).toMatchSnapshot()
   })
   test.each<TitreTypeId>(TitresTypesIds)('vérifie si un utilisateur super peut créer un titre de type %p', titreTypeId => {
     const user: User = { role: 'super', ...testBlankUser }
@@ -69,14 +80,14 @@ describe('canCreateTitre', () => {
   })
 
   test('vérifie si un utilisateur administrateur admin peut créer des titres', () => {
-    const result: { [key in AdministrationId]?: { [key in TitreTypeId]?: boolean } } = {}
+    const result: { [key in AdministrationId]?: { [key in TitreTypeId]?: true } } = {}
 
     Object.values(ADMINISTRATION_IDS).forEach(administrationId => {
       const user: User = { role: 'admin', administrationId, ...testBlankUser }
       for (const titreTypeid of TitresTypesIds) {
         const itCan = canCreateTitre(user, titreTypeid)
-        ;(result[administrationId] ??= {})[titreTypeid] = itCan
         if (itCan) {
+          ;(result[administrationId] ??= {})[titreTypeid] = itCan
           assertsCanCreateTitre(user, titreTypeid)
         } else {
           expect(() => assertsCanCreateTitre(user, titreTypeid)).toThrowError('permissions insuffisantes')
@@ -110,14 +121,16 @@ describe('canEditTitre', () => {
   })
 
   test('vérifie si un utilisateur administrateur admin peut modifier des titres', () => {
-    const result: { [key in AdministrationId]?: { [key in TitreTypeId]?: { [key in TitreStatutId]?: boolean } } } = {}
+    const result: { [key in AdministrationId]?: { [key in TitreTypeId]?: { [key in TitreStatutId]?: true } } } = {}
 
     Object.values(ADMINISTRATION_IDS).forEach(administrationId => {
       const user: User = { role: 'admin', administrationId, ...testBlankUser }
       for (const titreTypeid of TitresTypesIds) {
         for (const titreStatutId of titresStatutsArray) {
           const itCan = canEditTitre(user, titreTypeid, titreStatutId.id)
-          ;((result[administrationId] ??= {})[titreTypeid] ??= {})[titreStatutId.id] = itCan
+          if (itCan) {
+            ;((result[administrationId] ??= {})[titreTypeid] ??= {})[titreStatutId.id] = itCan
+          }
         }
       }
     })
