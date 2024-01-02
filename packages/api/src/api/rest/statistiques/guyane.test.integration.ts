3efc2e43108ef460013d3336ab9c12de6b95499c
vmaubert <v.maubert@code-troopers.com>
2024-01-02 10:43:36 +0100

    fix(test): corrige les tests suite au changement d’année

--- packages/api/src/api/rest/statistiques/guyane.test.integration.ts
+++ packages/api/src/api/rest/statistiques/guyane.test.integration.ts
@@ -2,6 +2,7 @@ import { restCall } from '../../../../tests/_utils/index.js'
 import { dbManager } from '../../../../tests/db-manager.js'
 import { expect, test, afterAll, beforeAll, vi } from 'vitest'
 import type { Pool } from 'pg'
+import { caminoAnneeValidator } from 'camino-common/src/date.js'
 
 console.info = vi.fn()
 console.error = vi.fn()
@@ -17,7 +18,7 @@ afterAll(async () => {
 })
 
 test('peut récupérer les statistiques de Guyane', async () => {
-  const tested = await restCall(dbPool, '/rest/statistiques/guyane', {}, undefined)
+  const tested = await restCall(dbPool, '/rest/statistiques/guyane/:annee', { annee: caminoAnneeValidator.parse('2023') }, undefined)
 
   expect(tested.statusCode).toBe(200)
   expect(tested.body).toMatchSnapshot()
