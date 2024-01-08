840a60e234c8c32705062ebdab46b6c565e11266
vmaubert <v.maubert@code-troopers.com>
2024-01-08 14:15:44 +0100

    feat(fichier): limite la taille des fichiers à 100 mo (#877)

--- packages/ui/src/components/_ui/dsfr-input-file.tsx
+++ packages/ui/src/components/_ui/dsfr-input-file.tsx
@@ -50,7 +50,7 @@ export const InputFile = caminoDefineComponent<Props>(['accept', 'uploadFile'], 
     <div class="fr-upload-group" style={{ opacity: dragHover.value ? '20%' : '100%' }} onDragover={onDragHover} onDragleave={onDragLeave} onDrop={dropFile}>
       <label class="fr-label" for="file-upload">
         Ajouter un fichier
-        <span class="fr-hint-text">Taille maximale : 30 Mo. Formats supportés : {props.accept.join(', ')}.</span>
+        <span class="fr-hint-text">Taille maximale : 100 Mo. Formats supportés : {props.accept.join(', ')}.</span>
       </label>
       {/* @ts-ignore files n'est pas reconnu par le typage... */}
       <input files={inputValue.value} class="fr-upload" type="file" id="file-upload" name="file-upload" accept={props.accept.map(a => `.${a}`).join(',')} onChange={uploadFile} />
