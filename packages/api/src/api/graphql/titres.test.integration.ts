935076f9f720dd8689b414e893e62960220983a3
Bitard Michaël <bitard.michael@gmail.com>
2024-02-01 16:33:01 +0100

    fix group by stuff

--- packages/api/src/api/graphql/titres.test.integration.ts
+++ packages/api/src/api/graphql/titres.test.integration.ts
@@ -1,3 +1,4 @@
+/* eslint-disable sql/no-unsafe-query */
 import { dbManager } from '../../../tests/db-manager.js'
 import { graphQLCall, queryImport } from '../../../tests/_utils/index.js'
 import { titreCreate } from '../../database/queries/titres.js'
@@ -9,13 +10,19 @@ import { toCaminoDate } from 'camino-common/src/date.js'
 
 import { vi, afterEach, afterAll, beforeAll, describe, test, expect } from 'vitest'
 import type { Pool } from 'pg'
+import { entrepriseUpsert } from '../../database/queries/entreprises.js'
+import { newEntrepriseId } from 'camino-common/src/entreprise.js'
+import { communeIdValidator } from 'camino-common/src/static/communes.js'
+import type { Knex } from 'knex'
 
 console.info = vi.fn()
 console.error = vi.fn()
 let dbPool: Pool
+let knexStuff: Knex
 beforeAll(async () => {
-  const { pool } = await dbManager.populateDb()
+  const { pool, knex } = await dbManager.populateDb()
   dbPool = pool
+  knexStuff = knex
 })
 afterEach(async () => {
   await dbManager.reseedDb()
@@ -267,6 +274,152 @@ describe('titre', () => {
         { id: 'titre-id-demarche-id-dpu' },
       ])
     )
+  })
+})
+
+// query TitresByEntrerise($entreprisesIds: [ID!]) {  titres(entreprisesIds: $entreprisesIds) { elements { id  } total }}
+describe('titres', () => {
+  const entrepriseId1 = newEntrepriseId('fr-12341234')
+  const nomRef = 'MA_REF'
+  const communeId = communeIdValidator.parse('31200')
+  const titre: ITitre = {
+    id: newTitreId('titre-id'),
+    nom: 'mon titre',
+    typeId: 'arm',
+    titreStatutId: 'ind',
+    references: [{ referenceTypeId: 'brg', nom: nomRef }],
+    publicLecture: true,
+    propsTitreEtapesIds: { titulaires: 'titre-id-demarche-id-dpu', points: 'titre-id-demarche-id-dpu' },
+    demarches: [
+      {
+        id: newDemarcheId('titre-id-demarche-id'),
+        titreId: newTitreId('titre-id'),
+        typeId: 'oct',
+        statutId: 'acc',
+        publicLecture: true,
+        etapes: [
+          {
+            id: newEtapeId('titre-id-demarche-id-dpu'),
+            typeId: 'dpu',
+            ordre: 0,
+            titreDemarcheId: newDemarcheId('titre-id-demarche-id'),
+            statutId: 'acc',
+            date: toCaminoDate('2020-02-02'),
+            administrationsLocales: ['dea-guyane-01'],
+            titulaires: [{ id: entrepriseId1 }],
+            communes: [{ id: communeId }],
+          },
+        ],
+      },
+    ],
+  }
+
+  test('peut filtrer des titres par titulaires/amodiataires', async () => {
+    await entrepriseUpsert({
+      id: entrepriseId1,
+      nom: `${entrepriseId1}`,
+      etablissements: [],
+      archive: false,
+    })
+
+    await titreCreate(titre, {})
+
+    const res = await graphQLCall(
+      dbPool,
+      `query TitresByEntreprise($entreprisesIds: [ID]) {  titres(entreprisesIds: $entreprisesIds) { elements { id  } total }}`,
+      { entreprisesIds: [entrepriseId1] },
+      {
+        role: 'admin',
+        administrationId: ADMINISTRATION_IDS['OFFICE NATIONAL DES FORÊTS'],
+      }
+    )
+
+    expect(res.body.errors).toBe(undefined)
+    expect(res.body.data).toMatchInlineSnapshot(`
+      {
+        "titres": {
+          "elements": [
+            {
+              "id": "titre-id",
+            },
+          ],
+          "total": 1,
+        },
+      }
+    `)
+  })
+
+  test('peut filtrer des titres par references', async () => {
+    await entrepriseUpsert({
+      id: entrepriseId1,
+      nom: `${entrepriseId1}`,
+      etablissements: [],
+      archive: false,
+    })
+
+    await titreCreate(titre, {})
+
+    const res = await graphQLCall(
+      dbPool,
+      `query TitresByReferences($references: String) {  titres(references: $references) { elements { id  } total }}`,
+      { references: nomRef },
+      {
+        role: 'admin',
+        administrationId: ADMINISTRATION_IDS['OFFICE NATIONAL DES FORÊTS'],
+      }
+    )
+
+    expect(res.body.errors).toBe(undefined)
+    expect(res.body.data).toMatchInlineSnapshot(`
+      {
+        "titres": {
+          "elements": [
+            {
+              "id": "titre-id",
+            },
+          ],
+          "total": 1,
+        },
+      }
+    `)
+  })
+
+  test('peut filtrer des titres par communes', async () => {
+    await entrepriseUpsert({
+      id: entrepriseId1,
+      nom: `${entrepriseId1}`,
+      etablissements: [],
+      archive: false,
+    })
+
+    const nomCommune = 'NOM DE COMMUNE'
+
+    await titreCreate(titre, {})
+    await knexStuff.raw(`insert into communes (id, nom) values ('${communeId}', '${nomCommune}')`)
+
+    const res = await graphQLCall(
+      dbPool,
+      `query TitresByCommunes($communes: String) {  titres(communes: $communes) { elements { id  } total }}`,
+      { communes: nomCommune },
+      {
+        role: 'admin',
+        administrationId: ADMINISTRATION_IDS['OFFICE NATIONAL DES FORÊTS'],
+      }
+    )
+
+    expect(res.body.errors).toBe(undefined)
+    expect(res.body.data).toMatchInlineSnapshot(`
+      {
+        "titres": {
+          "elements": [
+            {
+              "id": "titre-id",
+            },
+          ],
+          "total": 1,
+        },
+      }
+    `)
   })
 })
 
