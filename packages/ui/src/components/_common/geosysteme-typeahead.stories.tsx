961a72c39200185007638fa4001425e664d02dc1
Bitard Michaël <bitard.michael@gmail.com>
2024-02-07 16:29:40 +0100

    feat(geosysteme): uniformise la sélection du géosystème dans l'application

---
+++ packages/ui/src/components/_common/geosysteme-typeahead.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryFn } from '@storybook/vue3'
+import { GeoSystemeTypeahead } from './geosysteme-typeahead'
+import { action } from '@storybook/addon-actions'
+
+const meta: Meta = {
+  title: 'Components/Common/GeoSystemeTypeahead',
+  // @ts-ignore
+  component: GeoSystemeTypeahead,
+}
+export default meta
+
+const geoSystemeSelected = action('geoSystemeSelected')
+
+export const Default: StoryFn = () => <GeoSystemeTypeahead geoSystemeSelected={geoSystemeSelected} />
