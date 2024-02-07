9d8213659d8f888ca2911d02de3b1219f8a96ead
vmaubert <v.maubert@code-troopers.com>
2024-02-07 10:37:10 +0100

    refactor(activites): supprime un mock dans les tests des activités

--- packages/api/src/business/processes/titres-activites-relance-send.test.ts
+++ packages/api/src/business/processes/titres-activites-relance-send.test.ts
@@ -1,32 +1,29 @@
-import { titresActivitesGet } from '../../database/queries/titres-activites.js'
-
-import TitresActivites from '../../database/models/titres-activites.js'
-import { titresActivitesRelanceSend } from './titres-activites-relance-send.js'
+import { checkDateAndSendEmail } from './titres-activites-relance-send.js'
 import { emailsWithTemplateSend } from '../../tools/api-mailjet/emails.js'
 import { EmailTemplateId } from '../../tools/api-mailjet/types.js'
-import { vi, describe, expect, test } from 'vitest'
-import { toCaminoDate } from 'camino-common/src/date.js'
-vi.mock('../../database/queries/titres-activites', () => ({
-  titresActivitesGet: vi.fn(),
-}))
+import { vi, describe, expect, test, afterEach } from 'vitest'
+import { getCurrent, toCaminoDate } from 'camino-common/src/date.js'
+import { ITitreActivite } from '../../types.js'
 
 vi.mock('../../tools/api-mailjet/emails', () => ({
   __esModule: true,
   emailsWithTemplateSend: vi.fn().mockImplementation(a => a),
 }))
 
-const titresActivitesGetMock = vi.mocked(titresActivitesGet, true)
 const emailsWithTemplateSendMock = vi.mocked(emailsWithTemplateSend, true)
 
 console.info = vi.fn()
 
 describe('relance les opérateurs des activités qui vont se fermer automatiquement', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   test('envoie un email aux opérateurs', async () => {
     const date = '2022-01-01'
 
     const email = 'toto.huhu@foo.com'
 
-    titresActivitesGetMock.mockResolvedValue([
+    const activites = [
       {
         date,
         typeId: 'gra',
@@ -34,21 +31,21 @@ describe('relance les opérateurs des activités qui vont se fermer automatiquem
           titulaires: [{ utilisateurs: [{ email: 'toto.huhu@foo.com' }] }],
         },
       },
-    ] as TitresActivites[])
-    const titresActivites = await titresActivitesRelanceSend(toCaminoDate('2022-03-18'))
+    ] as ITitreActivite[]
+    const titresActivites = await checkDateAndSendEmail(toCaminoDate('2022-03-18'), activites)
 
     expect(emailsWithTemplateSendMock).toBeCalledWith([email], EmailTemplateId.ACTIVITES_RELANCE, expect.any(Object))
     expect(titresActivites.length).toEqual(1)
   })
 
   test('n’envoie pas d’email aux opérateurs', async () => {
-    titresActivitesGetMock.mockResolvedValue([
+    const activites = [
       {
         date: '1000-01-01',
         typeId: 'gra',
       },
-    ] as TitresActivites[])
-    const titresActivites = await titresActivitesRelanceSend()
+    ] as ITitreActivite[]
+    const titresActivites = await checkDateAndSendEmail(getCurrent(), activites)
 
     expect(titresActivites.length).toEqual(0)
   })
