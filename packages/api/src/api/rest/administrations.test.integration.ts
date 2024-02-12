7cd7487fe27b119a7768e01547a6654d92b17847
vmaubert <v.maubert@code-troopers.com>
2024-02-12 10:36:08 +0100

    fix tests

--- packages/api/src/api/rest/administrations.test.integration.ts
+++ packages/api/src/api/rest/administrations.test.integration.ts
@@ -1,17 +1,29 @@
-import { restCall } from '../../../tests/_utils/index.js'
+import { restCall, userGenerate } from '../../../tests/_utils/index.js'
 import { dbManager } from '../../../tests/db-manager.js'
 import { expect, test, describe, afterAll, beforeAll, vi } from 'vitest'
 import type { Pool } from 'pg'
 import { HTTP_STATUS } from 'camino-common/src/http.js'
 import { TestUser } from 'camino-common/src/tests-utils.js'
 import { ADMINISTRATION_IDS } from 'camino-common/src/static/administrations.js'
+import { toCaminoDate } from 'camino-common/src/date.js'
 
 console.info = vi.fn()
 console.error = vi.fn()
 let dbPool: Pool
 beforeAll(async () => {
-  const { pool } = await dbManager.populateDb()
+  const { knex: knexInstance, pool } = await dbManager.populateDb()
   dbPool = pool
+
+  await knexInstance('utilisateurs').insert({
+    id: 'deletedUserId',
+    prenom: 'test',
+    nom: 'test',
+    email: null,
+    keycloak_id: null,
+    role: 'lecteur',
+    administrationId: 'dea-guyane-01',
+    dateCreation: toCaminoDate('2022-05-12'),
+  })
 })
 
 afterAll(async () => {
@@ -33,20 +45,27 @@ describe('getAdministrationUtilisateurs', () => {
     [{ role: 'defaut' }, false],
     [undefined, false],
   ])('utilisateur %s peur voir les utilisateurs: %s', async (user, lecture) => {
+    await userGenerate({ role: 'defaut' })
+    await userGenerate({ role: 'admin', administrationId: 'dea-guyane-01' })
+    await userGenerate({ role: 'admin', administrationId: 'dea-reunion-01' })
+
     const tested = await restCall(dbPool, '/rest/administrations/:administrationId/utilisateurs', { administrationId: 'dea-guyane-01' }, user)
 
     if (lecture) {
       expect(tested.statusCode).toBe(HTTP_STATUS.HTTP_STATUS_OK)
+      expect(tested.body).toEqual([
+        {
+          administrationId: 'dea-guyane-01',
+          administration_id: 'dea-guyane-01',
+          email: 'admin-user-dea-guyane-01@camino.local',
+          id: 'admin-user-dea-guyane-01',
+          nom: 'nom-admin',
+          prenom: 'prenom-admin',
+          role: 'admin',
+        },
+      ])
     } else {
       expect(tested.statusCode).toBe(HTTP_STATUS.HTTP_STATUS_FORBIDDEN)
     }
-  })
-
-  test('ne récupère pas les utilisateurs sans keycloak_id', () => {
-    throw new Error('TODO')
-  })
-
-  test('ne récupère pas que les utilisateurs de l’administration', () => {
-    throw new Error('TODO')
   })
 })
