882dd69b612530487674c0d63af54fbaec21bf03
vmaubert <v.maubert@code-troopers.com>
2024-02-21 11:08:49 +0100

    fix(filtre): le bouton pour valider les filtres ne fonctionne pas si on ne modifie pas les filtres (#1022)

--- packages/ui/src/components/_ui/filters/filters.tsx
+++ packages/ui/src/components/_ui/filters/filters.tsx
@@ -385,7 +385,7 @@ export const Filters = defineComponent((props: Props) => {
             </div>
 
             <div class="dsfr">
-              <CaminoRouterLink class="fr-btn fr-mb-2w" to={urlQuery.value} title="Valider les filtres">
+              <CaminoRouterLink class="fr-btn fr-mb-2w" to={urlQuery.value} title="Valider les filtres" anchorHTMLAttributes={{ onClick: props.toggle }}>
                 Valider
               </CaminoRouterLink>
             </div>
