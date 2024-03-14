edf579c79b6c36150559e2a6a5303a6f6bd17362
vmaubert <v.maubert@code-troopers.com>
2024-03-14 16:04:11 +0100

    supprime console.log

--- packages/ui/src/components/demarche/demarche-map.tsx
+++ packages/ui/src/components/demarche/demarche-map.tsx
@@ -315,10 +315,9 @@ export const DemarcheMap = defineComponent<Props>(props => {
   watch(
     () => props.perimetre,
     () => {
-      (map.value?.getSource(contoursSourceName) as GeoJSONSource).setData(props.perimetre.geojson4326_perimetre)
+      ;(map.value?.getSource(contoursSourceName) as GeoJSONSource).setData(props.perimetre.geojson4326_perimetre)
       ;(map.value?.getSource(pointsSourceName) as GeoJSONSource).setData(points.value)
       ;(map.value?.getSource(foragesSourceName) as GeoJSONSource).setData(forages.value)
-      console.log('update perimetre', forages.value)
     }
   )
 
