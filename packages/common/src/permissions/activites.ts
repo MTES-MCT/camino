c28930a118b826bbd4a36a374dcf79a851e58095
Bitard Michaël <bitard.michael@gmail.com>
2024-03-18 10:20:37 +0100

    feat(activités): les préfectures peuvent voir les rapport d'activités des titres dont ils sont administration locale

--- packages/common/src/permissions/activites.ts
+++ packages/common/src/permissions/activites.ts
@@ -16,8 +16,7 @@ export const canReadActivites = (user: User) =>
   isSuper(user) ||
   isEntreprise(user) ||
   (isAdministration(user) &&
-    ([ADMINISTRATION_TYPE_IDS.MINISTERE, ADMINISTRATION_TYPE_IDS.DEAL, ADMINISTRATION_TYPE_IDS.DREAL].includes(Administrations[user.administrationId].typeId) ||
-      user.administrationId === ADMINISTRATION_IDS['PRÉFECTURE - GUYANE']))
+    [ADMINISTRATION_TYPE_IDS.MINISTERE, ADMINISTRATION_TYPE_IDS.DEAL, ADMINISTRATION_TYPE_IDS.DREAL, ADMINISTRATION_TYPE_IDS.PREFECTURE].includes(Administrations[user.administrationId].typeId))
 
 export const canDeleteActiviteDocument = (activiteDocumentTypeId: ActiviteDocumentTypeId, activiteTypeId: ActivitesTypesId, activiteStatutId: ActivitesStatutId) => {
   const documentType = [...activitesTypesDocumentsTypes[activiteTypeId]].find(({ documentTypeId }) => documentTypeId === activiteDocumentTypeId)
@@ -79,6 +78,10 @@ export const canEditActivite = async (
   }
 
   if (isAdministrationAdmin(user) || isAdministrationEditeur(user)) {
+    if (Administrations[user.administrationId].typeId === ADMINISTRATION_TYPE_IDS.PREFECTURE && user.administrationId !== ADMINISTRATION_IDS['PRÉFECTURE - GUYANE']) {
+      return false
+    }
+
     return isGestionnaire(user.administrationId, await typeId()) || (await administrations()).includes(user.administrationId)
   }
 
