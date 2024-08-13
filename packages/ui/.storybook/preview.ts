db155e824d5e8f4a7065ca7a8b449e4919381966
vmaubert <github@vcmb.dev>
2024-08-13 15:45:57 +0200

    chore(storybook): passe à la dernière version de storybook

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -20,7 +20,6 @@ export const decorators: Decorator[] = [
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
