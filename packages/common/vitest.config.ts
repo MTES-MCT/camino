aaa638f9e0b6ad237fcfd13874f80bb0c6264fe6
vmaubert <github@vcmb.dev>
2024-09-10 15:47:56 +0200

    chore(npm): met à jour vitest

--- packages/common/vitest.config.ts
+++ packages/common/vitest.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
       thresholds: {
         // the endgame is to put thresholds at 100 and never touch it again :)
         autoUpdate: true,
-        branches: 89.8,
+        branches: 89.96,
         functions: 79.52,
-        lines: 95.85,
-        statements: 95.85,
+        lines: 96.14,
+        statements: 96.14,
         perFile: false,
       },
     },
