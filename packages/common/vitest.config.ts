9344968101819e0918f51adbcc09c0723ee9b9ba
Anis Safine Laget <anis@poensis.fr>
2024-09-16 16:24:46 +0200

    fix coverage

--- packages/common/vitest.config.ts
+++ packages/common/vitest.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
       thresholds: {
         // the endgame is to put thresholds at 100 and never touch it again :)
         autoUpdate: true,
-        branches: 91.57,
-        functions: 83.46,
-        lines: 96.73,
-        statements: 96.73,
+        branches: 91.81,
+        functions: 83.85,
+        lines: 96.81,
+        statements: 96.81,
         perFile: false,
       },
     },
